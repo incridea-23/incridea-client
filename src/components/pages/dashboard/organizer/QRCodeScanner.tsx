cae744c21d39c5d73f4a1c24a26bbf109aaeb139
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-02 22:22:47 +0530

    feat(dashboard): mark attendance & scanning

--- src/components/pages/dashboard/organizer/QRCodeScanner.tsx
+++ src/components/pages/dashboard/organizer/QRCodeScanner.tsx
@@ -1,5 +1,9 @@
 import { useZxing } from 'react-zxing';
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { OrganizerMarkAttendanceDocument } from '@/src/generated/generated';
+import Button from '@/src/components/button';
+import { toast, Toaster } from 'react-hot-toast';
 
 export const QRCodeScanner: React.FC = ({}) => {
   const [result, setResult] = useState<string | null>(null);
@@ -14,11 +18,53 @@ export const QRCodeScanner: React.FC = ({}) => {
     },
   });
 
+  const [markAttendance, { loading: AttendanceLoading }] = useMutation(
+    OrganizerMarkAttendanceDocument,
+    {
+      refetchQueries: ['TeamsByRound'],
+      awaitRefetchQueries: true,
+    }
+  );
+
   return (
     <div className="flex flex-col items-center">
+      <Toaster />
       <video className="w-full border border-gray-500 rounded-lg" ref={ref} />
       <div className="mt-4">
-        {result && <p className="text-xl text-green-500">{result}</p>}
+        {result && (
+          <div className="flex flex-col items-center">
+            <p className="text-xl text-green-500">
+              <span className="font-bold">Team ID:</span> {result}
+            </p>
+            <Button
+              onClick={() => {
+                markAttendance({
+                  variables: {
+                    teamId: result,
+                    attended: true,
+                  },
+                }).then((res) => {
+                  if (
+                    res.data?.organizerMarkAttendance.__typename === 'Error'
+                  ) {
+                    toast.error('Not a valid team ID');
+                  }
+                  if (
+                    res.data?.organizerMarkAttendance.__typename ===
+                    'MutationOrganizerMarkAttendanceSuccess'
+                  ) {
+                    toast.success('Attendance marked');
+                  }
+                });
+              }}
+              disabled={AttendanceLoading}
+              intent={'primary'}
+              className="mt-2"
+            >
+              Mark Present
+            </Button>
+          </div>
+        )}
         {error && (
           <p className="text-xl text-red-500">
             {error && !result && 'No QR Code in sight'}
