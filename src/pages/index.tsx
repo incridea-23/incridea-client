69393dc62ee1f56b5a1034b207b39afef22e0da8
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-11 16:23:33 +0530

    feat: navbar and sky gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 // @refresh reset
-import { type NextPage } from "next";
-import { signIn, signOut } from "next-auth/react";
-import { makePayment } from "../utils/razorpay";
-import { useAuth } from "../hooks/useAuth";
-import Link from "next/link";
-import Rive, { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
+import { type NextPage } from 'next';
+import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
+import Navbar from '../components/navbar';
 
 const Home: NextPage = () => {
-  const { user, loading, error, status } = useAuth();
-  const { rive, RiveComponent } = useRive({
+  const { RiveComponent } = useRive({
     src: `assets/rive/wave.riv/`,
-    stateMachines: ["State Machine 1"],
+    stateMachines: ['State Machine 1'],
     autoplay: true,
     layout: new Layout({
       fit: Fit.FitWidth,
@@ -18,73 +14,17 @@ const Home: NextPage = () => {
     }),
   });
 
-  if (loading)
-    return (
-      <div className="h-screen w-screen flex justify-center items-center">
-        Loading...
-      </div>
-    );
   return (
-    <div className="overflow-x-hidden ">
-      <RiveComponent className="w-auto h-screen " />
+    <div className="overflow-x-hidden">
+      <div className="bg-gradient-to-b from-[#A4E4FD] to-[#006DC5]">
+        <Navbar />
+        <RiveComponent className="w-auto h-screen " />
+      </div>
       <div className="bg-gradient-to-b h-[200vh] from-[#5CA3AD] to-[#1b5b94]">
-        <Prof user={user} />
+        {/* Body */}
       </div>
     </div>
   );
 };
 
 export default Home;
-
-const Prof = ({ user }: any) => (
-  <main className="h-screen w-screen flex justify-center items-center flex-col gap-5">
-    <div className="text-2xl border-b border-gray-400">Incridea &apos;23</div>
-    <div>
-      <div className="flex flex-col gap-3">
-        {status === "authenticated" ? (
-          <div className="text-center space-y-2 text-md font-sans">
-            <div className="text-xl font-semibold ">Welcome {user?.name}</div>
-            <div className="text-sm font-light">Signed in as {user?.email}</div>
-            <div className="text-center">
-              <div className="font-light border border-blue-500 rounded-full inline-block px-5">
-                {user?.role} {user?.role != "USER" && user?.id}
-              </div>
-            </div>
-            <div className="flex gap-5 justify-center">
-              {user?.role === "USER" && (
-                <button
-                  onClick={makePayment}
-                  className="bg-green-500 px-3 py-1 text-white rounded-md">
-                  Register
-                </button>
-              )}
-
-              <button
-                className="bg-red-500 px-3 py-1 text-white rounded-md"
-                onClick={() => signOut()}>
-                Sign Out
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div className="text-center space-y-2 space-x-3">
-            <div className="text-lg ">You are not logged in</div>
-            <button
-              onClick={() => signIn()}
-              className="bg-blue-500 text-white px-3 py-2 rounded-md">
-              Sign In
-            </button>
-            <Link href="/auth/signup">
-              <button className="bg-blue-500 text-white px-3 py-2 rounded-md">
-                Sign Up
-              </button>
-            </Link>
-          </div>
-        )}
-      </div>
-    </div>
-    <div className="text-center">
-      <div className="flex flex-col sm:flex-row items-center gap-3"></div>
-    </div>
-  </main>
-);
