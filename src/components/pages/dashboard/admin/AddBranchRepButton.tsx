986340fd39b884d9c6f2a350448caa93b6491428
AaronNazareth <4nm20cs005@nmamit.in>
2023-03-12 13:10:26 +0530

    feat: addBranpRep feature & addBranchRep Mutation

--- src/components/pages/dashboard/admin/AddBranchRepButton.tsx
+++ src/components/pages/dashboard/admin/AddBranchRepButton.tsx
@@ -1,18 +1,45 @@
 import Button from '@/src/components/button';
 import { FC } from 'react';
 import { IoAdd } from 'react-icons/io5';
+import { AddBranchRepDocument } from '@/src/generated/generated';
+import { useMutation } from '@apollo/client';
+import createToast from '@/src/components/toast';
 
 
 const AddBranchRepButton: FC<{
     branchId: string;
     userId: string;
 }> = ({ branchId, userId }) => {
+
+    //addBranchRep Mutation
+    const [addBranchRepMutation, { loading: addBranchRepLoading }] 
+                = useMutation(AddBranchRepDocument, {
+                    refetchQueries: ['Branches'],
+                    awaitRefetchQueries: true,
+                });
+
+    const handleAddBranchRep = (userId: string) => {
+        let promise = addBranchRepMutation({
+            variables: {
+            branchId: branchId,
+            userId: userId,
+            },
+        }).then((res) => {
+            if (res.data?.addBranchRep.__typename !== 'MutationAddBranchRepSuccess') {
+            return Promise.reject('Error could not add branch rep');
+            }
+        });
+        createToast(promise, 'Adding BranchRep...');
+        };
+
+
     return (
         <Button 
             intent="success"
             size="medium"
             className="flex gap-1 items-center disabled:opacity-50 disabled:cursor-not-allowed"
-            onClick={ () => console.log('Add Branch Rep') }
+            onClick={ () => handleAddBranchRep(userId) }
+            disabled={addBranchRepLoading} 
         >
             <IoAdd />
         </Button>
