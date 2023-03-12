1a35188a17c3d8a238e01fb1846ca2e1e4fad598
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-12 17:22:30 +0530

    feat: gallery peek component

--- src/components/galleryPeek/index.tsx
+++ src/components/galleryPeek/index.tsx
@@ -1,9 +1,52 @@
+import Image from 'next/image';
 import React, { FC } from 'react';
 
-const GalleryPeak: FC = () => {
-  const images = ['/ryoko.png', 'logo.png', '/assets/png/helm.png'];
+const GalleryPeek: FC = () => {
+  const images = [
+    'v1678613776/1.jpg',
+    'v1678613775/2.jpg',
+    'v1678613774/3.jpg',
+  ]; // TODO: Replace with actual images (top 3)
 
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center backdrop-filter backdrop-blur-lg">
+      <div className="w-[400%] h-52 border-t border-b border-gray-400 overflow-visible relative">
+        <div className="w-[200%] flex items-center h-52 justify-around absolute left-0 animate">
+          {images.map((i) => {
+            return (
+              <div
+                className="flex justify-center items-start w-[20rem]"
+                key={i}
+              >
+                <Image
+                  src={'https://res.cloudinary.com/dpfpk49oa/image/upload/' + i}
+                  alt="Gallery Image"
+                  width={500}
+                  height={300}
+                />
+                <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-5"></div>
+              </div>
+            );
+          })}
+          {images.map((i) => {
+            return (
+              <div
+                className="flex justify-center items-start w-[20rem]"
+                key={i}
+              >
+                <Image
+                  src={'https://res.cloudinary.com/dpfpk49oa/image/upload/' + i}
+                  alt="Gallery Image"
+                  width={500}
+                  height={300}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default GalleryPeak;
+export default GalleryPeek;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,8 @@ import Parallax from '../components/animation/parallax';
 import CountDown from '../components/countdown';
 import About from '../components/about';
 import GalleryPeek from '../components/galleryPeek';
+import Button from '../components/button';
+import Link from 'next/link';
 const Home: NextPage = () => {
   const { RiveComponent: LandingBg } = useRive({
     src: `assets/rive/landing-scene-bg.riv/`,
@@ -40,12 +42,20 @@ const Home: NextPage = () => {
         <LandingBg className="absolute  top-0 left-0 -z-10 w-screen  h-screen bg-gradient-to-bl from-yellow-100 to-sky-400" />
         <LandingWave className="w-auto h-screen " />
       </div>
-      <div className="bg-gradient-to-b h-[200vh] from-[#5CA3AD] to-[#1b5b94]">
+      <div className="bg-gradient-to-b h-[300vh] from-[#5CA3AD] to-[#1b5b94]">
         {/* Body */}
         <div className="h-[200px]"></div>
         <CountDown />
         <About />
-        <GalleryPeek />
+        <div className="-rotate-12 blur-sm">
+          <GalleryPeek />
+        </div>
+        <div className="rotate-[20deg]">
+          <GalleryPeek />
+        </div>
+        <Link href={'/gallery'}>
+          <Button className="mx-auto mt-20">View Gallery</Button>
+        </Link>
       </div>
     </div>
   );
