6679c882eddb980ad69ba8a9a1baa250528a1d8d
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-20 21:46:45 +0530

    refactor: minor changes

--- src/components/galleryPeek/index.tsx
+++ src/components/galleryPeek/index.tsx
@@ -13,7 +13,7 @@ const GalleryPeek: FC = () => {
   return (
     <>
       <div className="relative">
-        <div className="w-[200%] flex gap-1 lg:gap-5 py-2 lg:py-5 items-center absolute left-0 animate border border-t-2 border-b-2 border-gray-400 backdrop-filter">
+        <div className="w-[500%] lg:w-[200%] flex gap-1 lg:gap-5 py-2 lg:py-5 items-center absolute left-0 animate border border-t-2 border-b-2 border-gray-400 backdrop-filter">
           {images.map((i) => {
             return (
               <div className="flex justify-center items-start gap-5" key={i}>
