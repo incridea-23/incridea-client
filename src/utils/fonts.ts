5792473a1673ba9db21faeddfb27771d5d478ba3
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-22 23:55:04 +0530

    refactor: font change

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,11 +1,11 @@
-import { Cinzel, Cinzel_Decorative } from '@next/font/google';
+import { Poppins, Trade_Winds } from '@next/font/google';
 
-export const titleFont = Cinzel_Decorative({
-  weight: ['400', '700', '900'],
+export const titleFont = Trade_Winds({
+  weight: ['400'],
   subsets: ['latin'],
 });
 
-export const bodyFont = Cinzel({
-  weight: ['400', '700', '900'],
+export const bodyFont = Poppins({
+  weight: ['400'],
   subsets: ['latin'],
 });
