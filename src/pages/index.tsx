2a01b3afc5e4aaec29eab058773499ac94c90ccc
Kumar Ankush <000.kumarankush@gmail.com>
2023-03-23 16:04:19 +0530

    formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,12 +49,14 @@ const Home: NextPage = () => {
 
         <motion.div
           className="absolute -z-10  top-0 left-0   "
-          style={{ y: transformBg }}>
+          style={{ y: transformBg }}
+        >
           <LandingBg className="w-screen  h-screen " />
         </motion.div>
         <motion.div
           style={{ y: transformTitle }}
-          className="absolute  top-0 right-0 -z-10 backdrop-blur-[1.5px] text-white flex flex-col justify-center items-center w-screen min-h-screen">
+          className="absolute  top-0 right-0 -z-10 backdrop-blur-[1.5px] text-white flex flex-col justify-center items-center w-screen min-h-screen"
+        >
           <h1 className={`${titleFont.className} text-7xl `}>INCRIDEA</h1>
           <p className="text-xl font-semibold">Tides Of Change</p>
         </motion.div>
