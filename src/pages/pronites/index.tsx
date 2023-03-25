ccc4e592e289a83bb915977d519f657b2fd2a2c3
Keerthan N S <keerthanns091@gmail.com>
2023-03-24 22:07:59 -0800

    [add] header

--- src/pages/pronites/index.tsx
+++ src/pages/pronites/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Navbar from '../../components/navbar';
 import { useAuth } from '../../hooks/useAuth';
 import { motion } from 'framer-motion';
+import Image from "next/image";
 
 const Pronite: NextPage = () => {
     const { status, user, error, loading } = useAuth();
@@ -10,21 +11,15 @@ const Pronite: NextPage = () => {
             {/* Navbar */}
             <Navbar status={status} user={user} />
             {/* Header Part */}
-            <div className="h-[600px] w-full relative flex bg-black">
-                <video
-                autoPlay
-                loop
-                muted
-                className="object-cover object-center w-full h-full opacity-75"
-                >
-                <source src="ocean.mp4" type="video/mp4"></source>
-                </video>
+            {/* <div className="h-[600px] w-full relative flex bg-center bg-[url('https://i.postimg.cc/rwwtdkTQ/pronite-bg.png')] bg-cover"> */}
+            <div className="h-[600px] w-full relative flex bg-center bg-[url('/assets/png/pronite_bg.png')] bg-cover">
                 <motion.div
                 animate={{ y: [20, 0], opacity: [0, 1], repeatCount: 1 }}
                 transition={{ duration: 3 }}
-                className="absolute top-1/2 flex w-full justify-center flex-col"
+                className="absolute top-1/2 flex w-full justify-center flex-col z-50 py-4 px-2 md:py-2 md:px-8 bg-black/50"
+                // className="absolute top-1/2 flex w-full justify-center flex-col z-50"
                 >
-                <h1 className="text-4xl sm:text-6xl text-center">
+                <h1 className="text-4xl sm:text-6xl text-center neonText">
                     PRONITES
                 </h1>
                 <h2 className="text-2xl sm:text-4xl text-center">
@@ -37,6 +32,10 @@ const Pronite: NextPage = () => {
                 style={{ x: '-50%' }}
                 className="h-1 w-40 flex bg-gray-100 absolute bottom-8 left-1/2"
                 ></motion.div>
+            </div>
+            <div className="h-[600px] w-full absolute top-0 left-0 flex bg-center bg-primary-800/40"></div>
+            <div className="bg-gradient-to-br from-[#110141]  to-[#040c2b] pt-12">
+                
             </div>
         </div>
     );
