c25fdda03933327f142738a7cde0b09bb25bdca8
AaronNazareth <4nm20cs005@nmamit.in>
2023-03-25 19:38:36 +0530

    feat:added edit event

---
+++ src/components/pages/dashboard/admin/EditEvent.tsx
@@ -0,0 +1,16 @@
+import Button from '@/src/components/button';
+import { AiOutlineEdit, AiOutlineEye } from 'react-icons/ai';
+
+
+const EditEvent = () => {
+    return (
+        <div className = "mx-0.5">
+            <Button
+            >
+                <AiOutlineEdit /> 
+            </Button>
+        </div>
+    )
+}
+
+export default EditEvent;
