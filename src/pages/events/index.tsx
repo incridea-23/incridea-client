1a2cf68b3703b82fbe59e28c3d132de86933d61b
Kumar Ankush <000.kumarankush@gmail.com>
2023-03-25 16:19:48 +0530

    add: filters dropdown menu

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -5,11 +5,13 @@ import { events } from "@/src/utils/events";
 import Event from "@/src/components/event";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
+import { Menu } from "@headlessui/react";
 
 const Events: NextPage = () => {
   const { status, user } = useAuth();
   const [filter, setFilter] = useState("All");
   const [data, setData] = useState(events);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     if (filter === "All") {
@@ -20,14 +22,14 @@ const Events: NextPage = () => {
   }, [filter]);
 
   return (
-    <div className="bg-[#B2E2D7] h-[100vh] w-[100vw]">
+    <div className="bg-[#B2E2D7] h-[100vh] w-[100vw] relative">
       <Navbar status={status} user={user} />
       <h1
         className={`${titleFont.className} font-bold text-5xl underline tracking-wide text-center my-5 mt-[8rem] absolute w-screen text-white`}
       >
         EVENTS
       </h1>
-      <div className="flex justify-center space-x-5 translate-y-[13.5rem] w-screen eventNavigation">
+      <div className="md:flex justify-center lg:space-x-3 md:space-x-2 space-x-1 xl:space-x-5 translate-y-[13.5rem] xl:w-screen mx-auto md:w-[30vw] hidden eventNavigation font-semibold">
         <span onClick={() => setFilter("All")}>All</span>
         <span onClick={() => setFilter("CORE")}>CORE</span>
         <span onClick={() => setFilter("CSE")}>CSE</span>
@@ -40,6 +42,66 @@ const Events: NextPage = () => {
         <span onClick={() => setFilter("CIVIL")}>CIVIL</span>
         <span onClick={() => setFilter("ROBOTICS")}>ROBOTICS</span>
       </div>
+      <span className="absolute top-[12rem] right-[3rem] md:hidden bg-white px-4 py-2 rounded-md z-[100] w-[8rem]">
+        <Menu>
+          <Menu.Button>Filters</Menu.Button>
+          <Menu.Items className="flex flex-col divide-y-2 text-md mt-4">
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("All")}>All</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("CORE")}>CORE</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("CSE")}>CSE</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("ISE")}>ISE</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("AI/ML")}>AI/ML</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => <span onClick={() => setFilter("CC")}>CC</span>}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("ECE")}>ECE</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("EEE")}>EEE</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("MECH")}>MECH</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("CIVIL")}>CIVIL</span>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <span onClick={() => setFilter("ROBOTICS")}>ROBOTICS</span>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Menu>
+      </span>
       <div className="absolute mt-[16rem] flex justify-center w-screen bg-[#B2E2D7] ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-10">
           {data.map((event) => (
@@ -48,7 +110,7 @@ const Events: NextPage = () => {
         </div>
       </div>
       {data.length === 0 && (
-        <div className="mt-[25rem] text-2xl w-screen text-center text-[#6d878a]">
+        <div className="translate-y-[25rem] text-2xl w-screen text-center text-[#6d878a] bg-[#B2E2D7]">
           No Event Found !!
         </div>
       )}
