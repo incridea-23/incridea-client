f65548d4508fb660daf552a2c7974fb2a384036f
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-02 01:31:48 +0530

    fix: pronites snap scroll

--- src/pages/pronites/index.tsx
+++ src/pages/pronites/index.tsx
@@ -2,97 +2,63 @@ import { NextPage } from "next";
 import Navbar from "../../components/navbar";
 import { useAuth } from "../../hooks/useAuth";
 import { motion } from "framer-motion";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
-
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useHorizontalScroll, useHorizontalTouch } from "./useHorizontalScroll";
 
 const Pronite: NextPage = () => {
-  const { status, user, error, loading } = useAuth();
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    let sections = gsap.utils.toArray(".panel");
-
-    // gsap.to(sections, {
-    // xPercent: -100 * (sections.length - 1),
-    // ease: "none",
-    // scrollTrigger: {
-    //     trigger: ".container",
-    //     pin: true,
-    //     scrub: 1,
-    //     snap: 1 / (sections.length - 1),
-    //     end: "+=3500",
-    // }
-    // });
-    gsap.to(".containers", {
-        xPercent: -100, 
-        x: () => innerWidth,
-        ease: "none",
-        scrollTrigger: {
-          trigger: ".containers",
-          start: "top top",
-          end: () => innerWidth * 3,
-          scrub: true,
-          pin: true,
-          invalidateOnRefresh: true,
-          anticipatePin: 1
-        }
-      });
-      
-  }, []);
-  
-
+  const scrollRef = useHorizontalScroll();
+  useHorizontalTouch(scrollRef);
   return (
     <>
-        <div className="h-screen bg-gradient-to-bl overflow-x-auto overflow-y-hidden  from-indigo-200 via-sky-500 to-cyan-100">
-            {/* Navbar */}
-            {/* <Navbar status={status} user={user} /> */}
-            <div className="relative h-screen w-[400vw] z-20 flex overflow-y-hidden text-gray-100">
-                <div className="fixed -z-10 bottom-0 h-screen w-full bg-right-bottom bg-repeat-x bg-[url('/assets/svg/beach.svg')]" />
-                <div className="fixed z-10 pointer-events-none bottom-0 h-[45rem] w-screen bg-bottom bg-repeat-x bg-[url('/assets/svg/bushes.svg')]" />
-                <div className="h-screen w-screen flex justify-center items-center panel">
-                    <motion.div
-                        animate={{ y: [20, 0], opacity: [0, 1], repeatCount: 1 }}
-                        transition={{ duration: 3 }}
-                        className="flex flex-col w-full py-10 top-1/4 md:py-2 md:px-8 bg-black/50">
-                        <h1 className="text-4xl text-center sm:text-6xl">PRONITES</h1>
-                        <h2 className="text-2xl text-center sm:text-4xl">
-                        These are the nights that never die.
-                        </h2>
-                    </motion.div>
-                </div>
-                <div className="h-screen w-screen panel">
-                    <Image
-                        className="h-screen w-screen "
-                        src="/assets/svg/proBro.svg"
-                        width={100}
-                        height={1000}
-                        alt="proBro"
-                    />
-                </div>
-                <div className="h-screen w-screen panel">
-                    <Image
-                        className="h-screen w-screen"
-                        src="/assets/svg/women.svg"
-                        width={1000}
-                        height={1000}
-                        alt="women"
-                    />
-                </div>
-                <div className="h-screen w-screen panel">
-                    <Image
-                        className="h-screen w-screen"
-                        src="/assets/svg/raghav.svg"
-                        width={1000}
-                        height={1000}
-                        alt="raghav"
-                    />
-                </div>
-            </div>
-            
+      <div className="relative h-screen bg-gradient-to-bl   overflow-y-hidden  from-indigo-200 via-sky-500 to-cyan-100">
+        {/* Navbar */}
+        <div className="fixed top-0 bottom-0 h-screen w-full bg-right-bottom bg-repeat-x bg-[url('/assets/svg/beach.svg')]" />
+        <div className="fixed z-10 pointer-events-none bottom-0 h-[45rem] w-screen bg-bottom bg-repeat-x bg-[url('/assets/svg/bushes.svg')]" />
+        {/* <Navbar status={status} user={user} /> */}
+        <div
+          ref={scrollRef}
+          className="absolute top-0 snap-start snap-x snap-mandatory flex overflow-x-auto  h-screen w-screen mx:auto overflow-y-hidden text-gray-100">
+          <div className="snap-start min-w-full h-screen place-items-center   w-screen flex justify-center items-center">
+            <motion.div
+              animate={{ y: [20, 0], opacity: [0, 1], repeatCount: 1 }}
+              transition={{ duration: 3 }}
+              className="flex flex-col w-full py-10 top-1/4 md:py-2 md:px-8 ">
+              <h1 className="text-4xl text-center sm:text-6xl">PRONITES</h1>
+              <h2 className="text-2xl text-center sm:text-4xl">
+                These are the nights that never die.
+              </h2>
+            </motion.div>
+          </div>
+          <div className="snap-start  min-w-full h-screen place-items-center   w-screen">
+            <Image
+              className="  h-screen w-screen "
+              src="/assets/svg/proBro.svg"
+              width={100}
+              height={1000}
+              alt="proBro"
+            />
+          </div>
+          <div className="snap-start  min-w-full h-screen place-items-center   w-screen">
+            <Image
+              className="h-screen w-screen"
+              src="/assets/svg/women.svg"
+              width={1000}
+              height={1000}
+              alt="women"
+            />
+          </div>
+          <div className="snap-start  min-w-full h-screen place-items-center   w-screen">
+            <Image
+              className="h-screen w-screen"
+              src="/assets/svg/raghav.svg"
+              width={1000}
+              height={1000}
+              alt="raghav"
+            />
+          </div>
         </div>
+      </div>
     </>
   );
 };
---
+++ src/pages/pronites/useHorizontalScroll.tsx
@@ -0,0 +1,59 @@
+import { useRef, useEffect, RefObject } from "react";
+
+export function useHorizontalScroll() {
+  const elRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const el = elRef.current;
+    if (el) {
+      const onWheel = (e: WheelEvent) => {
+        if (e.deltaY === 0) return;
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft + e.deltaY * 7,
+          behavior: "smooth",
+        });
+      };
+      el.addEventListener("wheel", onWheel);
+      return () => el.removeEventListener("wheel", onWheel);
+    }
+  }, []);
+  return elRef;
+}
+
+interface Touch {
+  x: number;
+  y: number;
+}
+
+export function useHorizontalTouch(elRef: RefObject<HTMLDivElement>) {
+  useEffect(() => {
+    const el = elRef.current;
+    if (el) {
+      let touchStart: Touch | null = null;
+
+      const onTouchStart = (e: TouchEvent) => {
+        touchStart = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+      };
+
+      const onTouchMove = (e: TouchEvent) => {
+        if (!touchStart) return;
+        const touchEnd = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+        const dy = touchEnd.y - touchStart.y;
+        const dx = touchEnd.x - touchStart.x;
+
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft - dy - dx,
+          behavior: "smooth",
+        });
+      };
+
+      el.addEventListener("touchstart", onTouchStart);
+      el.addEventListener("touchmove", onTouchMove);
+      return () => {
+        el.removeEventListener("touchstart", onTouchStart);
+        el.removeEventListener("touchmove", onTouchMove);
+      };
+    }
+  }, [elRef]);
+}
