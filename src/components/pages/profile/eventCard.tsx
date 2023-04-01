382a13b24bdbe76e9e02cd3fca50159ddd14506e
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-04-01 18:21:19 +0530

    fix: event card

--- src/components/pages/profile/eventCard.tsx
+++ src/components/pages/profile/eventCard.tsx
@@ -1,17 +1,19 @@
 import { titleFont } from '@/src/utils/fonts';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
 import { IoLocationOutline } from 'react-icons/io5';
+import { RiNumbersLine } from 'react-icons/ri';
 
 const EventCard: FC<{ event: any }> = ({ event }) => {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      className="flex flex-col items-center justify-center p-4 my-4 bg-white rounded-lg shadow-lg bg-opacity-30 backdrop-blur-2xl max-w-2xl w-[300px]"
+      className="flex flex-col items-center justify-center my-4 bg-white rounded-lg shadow-lg bg-opacity-30 backdrop-blur-2xl max-w-2xl w-[300px]"
     >
-      {event?.image ? (
-        <div className="flex items-center justify-center w-full h-40 overflow-hidden rounded-lg">
+      <div className="flex items-center justify-center w-full h-40 overflow-hidden border-b border-gray-300 rounded-t">
+        {event?.image ? (
           <Image
             src={event?.image}
             alt={event?.name}
@@ -19,18 +21,27 @@ const EventCard: FC<{ event: any }> = ({ event }) => {
             height={200}
             className="rounded-lg"
           />
-        </div>
-      ) : (
-        <span className="flex items-center justify-center w-full h-40 text-xl font-bold text-center text-gray-800">
-          No Image Available.
-        </span>
-      )}
-      <div className="flex flex-col items-center justify-center pt-5">
-        <h1
-          className={`${titleFont.className} text-2xl font-bold text-center text-gray-800`}
+        ) : (
+          <span className="text-xl font-bold text-center text-gray-800">
+            No Image Available.
+          </span>
+        )}
+      </div>
+
+      <div className="pt-5">
+        <Link
+          href={`/events/${event?.id}`}
+          className={`${titleFont.className} text-2xl font-bold text-center text-gray-900 hover:text-gray-700`}
         >
           {event?.name}
-        </h1>
+        </Link>
+      </div>
+
+      <div className="flex justify-between w-full p-5">
+        <p className="flex items-center gap-1 text-lg font-medium text-center text-gray-800">
+          <RiNumbersLine />
+          {event?.rounds.length} Rounds
+        </p>
 
         <div className="flex items-center justify-center gap-1">
           <IoLocationOutline />
--- src/components/pages/profile/registeredEvents.tsx
+++ src/components/pages/profile/registeredEvents.tsx
@@ -1,7 +1,6 @@
 import { RegisterdEventsDocument } from '@/src/generated/generated';
 import { titleFont } from '@/src/utils/fonts';
 import { useQuery } from '@apollo/client';
-import TextAnimation from '../../animation/text';
 import Spinner from '../../spinner';
 import EventCard from './eventCard';
 
@@ -10,18 +9,18 @@ const UserEvents = () => {
 
   return (
     <section data-scroll-section className="min-h-screen">
-      <TextAnimation
-        text="Dive into action with your upcoming adventures!"
-        textStyle={`${titleFont.className} text-2xl lg:text-4xl font-bold text-center text-white`}
-        className="flex justify-center"
-      />
+      <h1
+        className={`${titleFont.className} text-2xl lg:text-4xl font-bold text-center text-white flex justify-center lg:max-w-full md:max-w-full max-w-sm`}
+      >
+        Dive into action with your upcoming adventures!
+      </h1>
       {loading && (
         <div className="flex items-center justify-center h-40">
           <Spinner size={'medium'} intent={'white'} />
         </div>
       )}
       {!loading && !error && events && (
-        <div className="flex flex-row space-x-5 flex-wrap items-center justify-center">
+        <div className="flex gap-5 flex-wrap items-center justify-center mt-5">
           {events?.registeredEvents.__typename ===
             'QueryRegisteredEventsSuccess' &&
             events?.registeredEvents.data?.map((event) => (
