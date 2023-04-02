6217ce70e20c1699a2900b5333718f6d29558b9c
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-02 15:04:49 +0530

    feat: Event page ISR

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,18 +7,26 @@ import { useState } from "react";
 import { titleFont } from "../utils/fonts";
 
 const SignIn: NextPage = () => {
-  const { query }: { query: { whichForm?: "signIn" | "resetPassword" | "signUp" } } =
+  const {
+    query,
+  }: { query: { whichForm?: "signIn" | "resetPassword" | "signUp" } } =
     useRouter();
-  const [whichForm, setWhichForm] = useState<"signIn" | "resetPassword" | "signUp">(
-    query.whichForm || "signIn"
-  );
+  const [whichForm, setWhichForm] = useState<
+    "signIn" | "resetPassword" | "signUp"
+  >(query.whichForm || "signIn");
 
   return (
     <div className="min-h-screen min-w-screen bg-cover bg-gradient-to-bl from-[#46aacf]  via-[#075985] to-[#2d6aa6]">
-      <div className={`hidden md:block w-[100vh] fixed top-1/2 -translate-y-1/2 transition-transform duration-500 -rotate-90 left-[47%] ${whichForm === 'signUp' ? '-translate-x-[110%]' : '-translate-x-1/2'}`}>
+      <div
+        className={`hidden md:block w-[100vh] fixed top-1/2 -translate-y-1/2 transition-transform duration-500 -rotate-90 left-[47%] ${
+          whichForm === "signUp" ? "-translate-x-[110%]" : "-translate-x-1/2"
+        }`}>
         <img src={"/wave.svg"} className="w-[100vh]" />
       </div>
-      <div className={`hidden md:block w-[100vh]  fixed top-1/2 -translate-y-1/2 transition-transform duration-500 rotate-90 right-[47%] ${whichForm !== 'signUp' ? 'translate-x-[110%]' : 'translate-x-1/2'}`}>
+      <div
+        className={`hidden md:block w-[100vh]  fixed top-1/2 -translate-y-1/2 transition-transform duration-500 rotate-90 right-[47%] ${
+          whichForm !== "signUp" ? "translate-x-[110%]" : "translate-x-1/2"
+        }`}>
         <img src={"/wave.svg"} className="w-[100vh]" />
       </div>
       <div className="flex w-screen h-screen">
@@ -27,9 +35,13 @@ const SignIn: NextPage = () => {
             whichForm === "signUp" && "md:translate-x-full"
           }`}>
           {whichForm === "signUp" ? (
-            <div className={`${titleFont.className} pl-12`}>Signup text/mascot</div>
+            <div className={`${titleFont.className} pl-12`}>
+              Signup text/mascot
+            </div>
           ) : (
-            <div className={`${titleFont.className} pr-12`}>Signin text/mascot</div>
+            <div className={`${titleFont.className} pr-12`}>
+              Signin text/mascot
+            </div>
           )}
         </div>
         <div
