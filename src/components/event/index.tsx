f6715b254db34a26c0b5faf4fce290beb9ccc0d5
Numan Naeem <numan.naeem9@gmail.com>
2023-04-02 21:13:27 +0530

    feat: events card and search bar

--- src/components/event/index.tsx
+++ src/components/event/index.tsx
@@ -1,62 +1,119 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Button from "../button";
+import { PublishedEventsQuery } from "@/src/generated/generated";
+import { titleFont } from "@/src/utils/fonts";
 import { AnimatePresence, motion } from "framer-motion";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import Button from "../button";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  IoCalendarClearOutline,
+  IoCalendarOutline,
+  IoCashOutline,
+  IoInformationOutline,
+  IoLocationOutline,
+  IoPeopleOutline,
+  IoPersonOutline,
+} from "react-icons/io5";
 
-interface EventProps {
-  data: {
-    id: string;
-    name: string;
-    date: string;
-    image: string;
-    link: string;
-    branch: string;
-    venue: string;
-    rounds: Number;
-    price: Number;
-  };
-}
-
-const Event = ({ data }: EventProps) => {
+const Event = ({ data }: { data: PublishedEventsQuery["publishedEvents"][0] }) => {
   const router = useRouter();
+
+  const getEventAttributes = () => {
+    let teamSizeText = "";
+    if (data.minTeamSize === data.maxTeamSize) {
+      if (data.minTeamSize !== 1) teamSizeText += `${data.minTeamSize} members per team`;
+    } else {
+      teamSizeText = `${data.minTeamSize} - ${data.maxTeamSize} members per team`;
+    }
+    return [
+      {
+        name: "Date",
+        text: data.rounds[0]?.date || "TBD",
+        Icon: IoCalendarOutline,
+      },
+      {
+        name: "Venue",
+        text: data.venue,
+        Icon: IoLocationOutline,
+      },
+      {
+        name: "Fees",
+        text: data.fees || "Free",
+        Icon: IoCashOutline,
+      },
+      {
+        name: "Type",
+        text: data.eventType[0] + data.eventType.slice(1).toLowerCase() + ' Event',
+        Icon: IoPersonOutline,
+      },
+      {
+        name: "Team Size",
+        text: teamSizeText,
+        Icon: IoPeopleOutline,
+      },
+    ];
+  };
 
   return (
     <AnimatePresence>
       <motion.div
         onClick={() => {
-          router.push(`events/${data.id}`);
+          router.push(
+            `events/${data.name.toLocaleLowerCase().split(" ").join("-")}-${data.id}`
+          );
         }}
         whileHover={{
-          translateY: -10,
-          // transition: { duration: 0.1 },
+          scale: 1.02,
+          transition: { duration: 0.1 },
         }}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         layout
         key={data.id}
-        className="bg-white/30 cursor-pointer p-4 rounded-md">
-        <motion.img
-          src={data.image}
-          alt={data.name}
-          className="w-full object-cover rounded-md"
-        />
-        <div className="flex justify-between items-end">
-          <div className="flex shrink-0 grow flex-col mt-3 xl:space-y-1.5">
-            <span className="text-gray-100 font-bold text-xl">{data.name}</span>
-            <span className="text-gray-100 text-sm font-semibold">
-              Date: {new Date(data.date).toLocaleDateString("en-UK")}
-            </span>
-            <span className="text-gray-100 text-sm font-semibold">
-              Venue : {data.venue}
-            </span>
-          </div>
-          <Link href={`events/${data.id}`}>
-            <Button>{data.price === 0 ? "Free" : `Rs ${data.price}`}</Button>
-          </Link>
+        className="bg-black/20 backdrop-blur-sm flex flex-col cursor-pointer p-4 rounded-sm">
+        <div className="relative grow">
+          {data.image ? (
+            <img
+              src={data.image}
+              alt={data.name}
+              width={500}
+              height={500}
+              className="w-full object-cover rounded-sm"
+            />
+          ) : (
+            <div className="h-full min-h-[200px] bg-gray-700 flex items-center justify-center italic text-gray-400 rounded-sm">
+              no image
+            </div>
+          )}
+          <span
+            className={`${titleFont.className} bg-gradient-to-t from-black/30 to-transparent p-2 pl-4 h-1/2 w-full flex items-end bottom-0 absolute drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.7)] text-gray-100 font-bold text-xl`}>
+            {data.name}
+          </span>
         </div>
+        <div className="flex flex-wrap mt-2 gap-1.5">
+          {getEventAttributes().map(
+            (attr) =>
+              attr.text && (
+                <div
+                  key={attr.name}
+                  className="flex px-3 py-2 text-gray-200 bg-gray-300/20 shrink-0 text-sm rounded-sm grow gap-1 items-center">
+                  {<attr.Icon />}
+                  <p className="leading-4">
+                    {/* <span className="font-semibold">{attr.name}: </span> */}
+                    {attr.text}
+                  </p>
+                </div>
+              )
+          )}
+        </div>
+        <Button noScaleOnHover className="hover:scale-0 shrink-0 mt-2">
+          <Link href={`/${data.name.toLowerCase().replaceAll(" ", "-")}-${data.id}}`}>
+            Register
+          </Link>
+        </Button>
       </motion.div>
     </AnimatePresence>
   );
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -6,8 +6,15 @@ import Event from "@/src/components/event";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { Menu } from "@headlessui/react";
+import { PublishedEventsDocument, PublishedEventsQuery } from "@/src/generated/generated";
+import Image from "next/image";
+import { client } from "@/src/lib/apollo";
+import SearchBox from "@/src/components/searchbox";
+import { AiOutlineSearch } from "react-icons/ai";
 
-const Events: NextPage = () => {
+const Events: NextPage<{ data: PublishedEventsQuery["publishedEvents"] }> = ({
+  data,
+}) => {
   const filters = [
     "ALL",
     "CORE",
@@ -22,83 +29,135 @@ const Events: NextPage = () => {
     "ROBOTICS",
   ];
   const { status, user } = useAuth();
-  const [currentFilter, setFilter] = useState<typeof filters[number]>("ALL");
-  const [data, setData] = useState(events);
-  const [isOpen, setIsOpen] = useState(false);
+  const [currentFilter, setCurrentFilter] = useState<typeof filters[number]>("ALL");
+  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    if (currentFilter === "ALL") {
-      setData(events);
+  const [filteredEvents, setFilteredEvents] = useState(data || []);
+  
+  const handleFilter = (filter: typeof filters[number]) => {
+    setQuery("");
+    setCurrentFilter(filter);
+    if (filter === "ALL") {
+      setFilteredEvents(data || []);
     } else {
-      setData(events.filter((event) => event.branch === currentFilter));
+      setFilteredEvents(data.filter((event) => event.branch.name === filter));
     }
-  }, [currentFilter]);
+  }
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    setCurrentFilter("ALL");
+    if (e.target.value === "") {
+      setFilteredEvents(data || []);
+    } else {
+      setFilteredEvents(
+        data.filter((event) => event.name.toLowerCase().includes(e.target.value.toLowerCase()))
+      );
+    }
+  };
 
   return (
-    <div className="bg-gradient-to-b from-[#46aacf]  via-[#075985] to-[#2d6aa6] min-h-screen relative">
+    <div className="bg-gradient-to-b  from-[#41acc9]  via-[#075985] to-[#2d6aa6] min-h-screen relative">
       <Navbar status={status} user={user} />
+      <Image
+        src="/assets/png/waterflare.png"
+        height={1000}
+        width={1000}
+        alt="flare"
+        className="absolute max-h-screen pointer-events-none opacity-50  top-0 right-0"
+      />
       <h1
         className={`${titleFont.className} font-bold text-5xl tracking-wide text-center pt-32 text-white`}>
         EVENTS
       </h1>
-      <div className="lg:flex justify-center gap-3 mt-10 mx-auto  hidden eventNavigation font-semibold">
-        {filters.map((filter) => (
-          <span
-            key={filter}
-            className={`${
-              filter === currentFilter ? "bg-gray-300/60" : "hover:bg-gray-300/40"
-            } text-white cursor-pointer rounded-full px-3 py-1`}
-            onClick={() => setFilter(filter)}>
-            {filter}
-          </span>
-        ))}
+      <div className="flex items-center gap-2 md:mx-10 mx-4 justify-between lg:flex-col lg:mx-auto mt-4">
+        <div className="relative lg:w-[800px] w-full">
+          <input
+            value={query}
+            onChange={handleSearch}
+            className="w-full pr-14 bg-black/30 placeholder:text-gray-200/70 focus:outline-none text-white rounded-sm  pl-3 p-2"
+            placeholder="Search away!"
+            type="text"
+          />
+          <AiOutlineSearch
+            size={"1.4rem"}
+            className="absolute right-3 top-2.5 text-gray-300/70"
+          />
+        </div>
+        <div className="lg:flex lg:w-[800px] justify-between gap-3 mt-4 mx-auto  hidden eventNavigation font-semibold">
+          {filters.map((filter) => (
+            <span
+              key={filter}
+              className={`${
+                filter === currentFilter ? "bg-black/20" : "hover:bg-black/10"
+              } text-white cursor-pointer rounded-sm px-3 py-1`}
+              onClick={() => handleFilter(filter)}>
+              {filter}
+            </span>
+          ))}
+        </div>
+        <div className="lg:hidden flex justify-center my-2 py-2 rounded-md">
+          <Menu as={"div"} className={"relative inline-block"}>
+            <Menu.Button
+              className={
+                "inline-flex bg-white/90 w-full justify-center rounded-full px-4 py-2 text-sm font-medium text-black"
+              }>
+              Filters
+            </Menu.Button>
+            <Menu.Items className="overflow-hidden pb-1.5 bg-white absolute z-[1] text-center right-0  top-0 rounded-md shadow-lg">
+              {filters.map((filter) => (
+                <Menu.Item key={filter}>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        currentFilter === filter ? "bg-red-300" : "bg-white"
+                      } text-black rounded-sm m-1.5 mb-0 w-32 px-3 py-2 text-sm`}
+                      onClick={() => handleFilter(filter)}>
+                      {filter}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Menu>
+        </div>
       </div>
-      <div className="lg:hidden flex justify-center my-2 px-4 py-2 rounded-md">
-        <Menu as={"div"} className={"relative inline-block"}>
-          <Menu.Button
-            className={
-              "inline-flex bg-white/90 w-full justify-center rounded-full px-4 py-2 text-sm font-medium text-black"
-            }>
-            Filters
-          </Menu.Button>
-          <Menu.Items className="overflow-hidden pb-1.5 bg-white absolute z-[1] text-center translate-x-1/2 right-1/2  top-0 rounded-md shadow-lg">
-            {filters.map((filter) => (
-              <Menu.Item key={filter}>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      currentFilter === filter ? "bg-sky-300" : "bg-white"
-                    } text-black rounded-sm m-1.5 mb-0 w-32 px-3 py-2 text-sm`}
-                    onClick={() => setFilter(filter)}>
-                    {filter}
-                  </button>
-                )}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Menu>
-      </div>
-      {currentFilter !== "ALL" && data.length > 0 && (
+      {currentFilter !== "ALL" && filteredEvents.length > 0 && (
         <div className="md:hidden flex mb-3 justify-center">
           <span className="text-gray-200  text-xs">
-            Displaying {data.length} {currentFilter} event(s)
+            Displaying {filteredEvents.length} {currentFilter} event(s)
           </span>
         </div>
       )}
       <div className="md:p-10 md:pt-10 pt-1 p-4 flex justify-center ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {data.map((event) => (
-            <Event data={event} key={event.id} />
-          ))}
-        </div>
+        {filteredEvents.length === 0 ? (
+          <div className="flex italic items-center justify-center min-h-[20rem] text-xl w-screen text-center text-gray-200/70">
+            <span>no events found</span>
+          </div>
+        ) : (
+          <div className="max-w-7xl w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {filteredEvents.map((event) => (
+              <Event data={event} key={event.id} />
+            ))}
+          </div>
+        )}
       </div>
-      {data.length === 0 && (
-        <div className="flex italic items-center justify-center min-h-[20rem] text-2xl w-screen text-center text-gray-200/70">
-          <span>no events found</span>
-        </div>
-      )}
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const { data: events } = await client.query({
+    query: PublishedEventsDocument,
+    fetchPolicy: "no-cache",
+  });
+
+  return {
+    props: {
+      data: events.publishedEvents,
+    },
+    revalidate: 60,
+  };
+}
 
 export default Events;
