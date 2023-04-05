c09848ee6b58a3e742559593ebcb3b82b0bde099
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-04-05 19:04:50 +0530

    feat: about sections

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,42 @@
+import { titleFont } from '@/src/utils/fonts';
+import { NextPage } from 'next';
+import Image from 'next/image';
+import 'locomotive-scroll/dist/locomotive-scroll.css';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import { useRef } from 'react';
+import AboutIncridea from '@/src/components/pages/about/AboutIncridea';
+import AboutCollege from '@/src/components/pages/about/AboutCollege';
+
+const About: NextPage = () => {
+  const containerRef = useRef(null);
+
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        smartphone: {
+          smooth: true,
+        },
+        tablet: {
+          smooth: true,
+        },
+      }}
+      watch={[]}
+      containerRef={containerRef}
+    >
+      <main
+        data-scroll-container
+        ref={containerRef}
+        className="bg-gradient-to-b from-[#5CA3AD]  via-[#1a6779] to-[#0e4450]"
+      >
+        {/* 1. About Incridea */}
+        <AboutIncridea />
+
+        {/* 2. About College */}
+        <AboutCollege />
+      </main>
+    </LocomotiveScrollProvider>
+  );
+};
+
+export default About;
