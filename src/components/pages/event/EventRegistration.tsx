0e6d5e3d2c73516c112ea0e48892270d6525e32d
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-04-12 19:41:42 +0530

    fix: multiple entry buttons

--- src/components/pages/event/EventRegistration.tsx
+++ src/components/pages/event/EventRegistration.tsx
@@ -1,23 +1,20 @@
 import { useAuth } from '@/src/hooks/useAuth';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Button from '../../button';
 import {
-  CreateTeamDocument,
   Event,
-  JoinTeamDocument,
   MyTeamDocument,
   QueryMyTeamSuccess,
   RegisterSoloEventDocument,
 } from '@/src/generated/generated';
-import Modal from '../../modal';
 import { useMutation, useQuery } from '@apollo/client';
 import createToast from '../../toast';
-import { teamIdToId } from '@/src/utils/id';
 import { makeTeamPayment } from '@/src/utils/razorpay';
 import Spinner from '../../spinner';
 import TeamCard from './TeamCard';
-import { useRouter } from 'next/router';
+import CreateTeamModal from './CreateTeamModal';
+import JoinTeamModal from './JoinTeamModal';
 
 function EventRegistration({
   eventId,
@@ -161,167 +158,3 @@ function EventRegistrationButton({
     }
   }
 }
-
-const CreateTeamModal = ({ eventId }: { eventId: Event['id'] }) => {
-  const [open, setOpen] = useState(false);
-  const [error, setError] = useState('');
-  const [createTeam, { loading, error: mutationError }] = useMutation(
-    CreateTeamDocument,
-    {
-      refetchQueries: ['MyTeam'],
-    }
-  );
-
-  const [name, setName] = useState('');
-  const handleCreateTeam = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const promise = createTeam({
-      variables: {
-        eventId: eventId,
-        name: name,
-      },
-      refetchQueries: ['MyTeam'],
-    }).then((res) => {
-      if (res.data?.createTeam.__typename === 'Error') {
-        setError(res.data.createTeam.message);
-      } else setOpen(false);
-    });
-    await createToast(promise, 'Creating Team');
-  };
-
-  return (
-    <>
-      <Button
-        className="w-full"
-        onClick={() => setOpen(true)}
-        intent={'primary'}
-      >
-        Create Team
-      </Button>
-      <Modal
-        onClose={() => {
-          setOpen(false);
-          setError('');
-        }}
-        showModal={open}
-        size="small"
-        title="Create Team"
-        rounded="md"
-      >
-        <form
-          onSubmit={handleCreateTeam}
-          className="gap-3 md:px-6 md:pb-6 px-5 pb-5  w-full  flex flex-col "
-        >
-          <div className="flex flex-col gap-2">
-            <label htmlFor="teamName" className="text-gray-300 font-semibod">
-              Team Name
-            </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              name="teamName"
-              id="teamName"
-              required
-              className="w-full bg-gray-800 rounded-sm px-2 py-1 focus:outline-none focus:ring ring-gray-500"
-            />
-          </div>
-          <Button type="submit" intent="success">
-            Create Team
-          </Button>
-          {error && (
-            <p className="text-red-800 bg-red-200 px-3 py-1 rounded-sm font-semibold">
-              {error}
-            </p>
-          )}
-        </form>
-      </Modal>
-    </>
-  );
-};
-
-const JoinTeamModal = () => {
-  const [open, setOpen] = useState(false);
-  const [error, setError] = useState('');
-  const [joinTeam, { loading, error: mutationError }] = useMutation(
-    JoinTeamDocument,
-    {
-      refetchQueries: ['MyTeam'],
-    }
-  );
-  const handleJoinTeam = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const promise = joinTeam({
-      variables: {
-        teamId: teamIdToId(teamId),
-      },
-    }).then((res) => {
-      if (res.data?.joinTeam.__typename === 'Error') {
-        setError(res.data.joinTeam.message);
-      } else {
-        setError('');
-        setOpen(false);
-      }
-    });
-    await createToast(promise, 'Joining Team');
-  };
-  const [teamId, setTeamId] = useState('');
-  const router = useRouter();
-  const {jointeam} = router.query;
-
-  useEffect(() => {
-    if (jointeam) {
-      setTeamId(jointeam as string);
-      setOpen(true);
-    }
-  }, [jointeam]);
-
-  return (
-    <>
-      <Button
-        className="w-full"
-        disabled={loading}
-        onClick={() => setOpen(true)}
-        intent={'primary'}
-      >
-        Join Team
-      </Button>
-      <Modal
-        onClose={() => setOpen(false)}
-        showModal={open}
-        size="small"
-        title="Join Team"
-        rounded="md"
-      >
-        <form
-          onSubmit={handleJoinTeam}
-          className="gap-3 md:px-6 md:pb-6 px-5 pb-5  w-full  flex flex-col "
-        >
-          <div className="flex flex-col gap-2">
-            <label htmlFor="teamName" className="text-gray-300 font-semibod">
-              Team Id
-            </label>
-            <input
-              type="text"
-              value={teamId}
-              onChange={(e) => setTeamId(e.target.value)}
-              name="teamName"
-              id="teamName"
-              placeholder="T23-10902"
-              required
-              className="w-full bg-gray-800 rounded-sm px-2 py-1 focus:outline-none focus:ring ring-gray-500"
-            />
-          </div>
-          <Button disabled={loading} type="submit" intent="success">
-            Join Team
-          </Button>
-          {error && (
-            <p className="text-red-800 bg-red-200 px-3 py-1 rounded-sm font-semibold">
-              {error}
-            </p>
-          )}
-        </form>
-      </Modal>
-    </>
-  );
-};
