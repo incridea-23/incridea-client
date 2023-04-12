aaf6df9929dedc90039cd77c2592b0a26df850b6
Kumar Ankush <000.kumarankush@gmail.com>
2023-04-12 14:15:21 +0530

    add: reset password form

--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -1,12 +1,59 @@
-import { NextPage } from 'next';
-import ResetPassword from '@/src/components/form/resetPassword';
+import ResetPassword from "../../components/form/resetPassword";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { titleFont } from "../../utils/fonts";
 
-const ResetPasswordPage: NextPage = () => {
+const Reset: NextPage = () => {
+  const {
+    query,
+  }: {
+    query: {
+      whichForm?: "signIn" | "resetPassword" | "signUp" | "resendEmail";
+    };
+  } = useRouter();
+  const [whichForm, setWhichForm] = useState<
+    "signIn" | "resetPassword" | "signUp" | "resendEmail"
+  >(query.whichForm || "signIn");
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <ResetPassword />
+    <div className="min-h-screen pt-20 md:pt-0 overflow-x-hidden min-w-screen bg-[#f3e9d1]">
+      <div
+        className={`w-screen transition-transform duration-500 flex ${
+          whichForm === "signUp" ? "md:translate-x-0" : "md:-translate-x-[50vw]"
+        }`}
+      >
+        <div className="hidden md:flex h-full grow">
+          <div
+            className={`${titleFont.className} text-center w-[50vw] items-center justify-center text-white/70 flex bg-gradient-to-r to-[#3baee7] from-[#144f6d] `}
+          >
+            <p className="text-2xl -rotate-90">The adventure ahead awaits!</p>
+          </div>
+          <img src={"/login-wave.png"} className="h-screen" />
+        </div>
+        <div
+          className={`shrink-0 md:w-[50vw] w-screen flex items-center justify-center bg-[#f3e9d1] text-[#6f5925] `}
+        >
+          <div className="md:max-w-md flex flex-col h-full min-h-screen">
+            <div className="p-6 md:py-10 grow">
+              <ResetPassword />
+            </div>
+            <img src={"/login-wave.svg"} className="md:hidden block" />
+          </div>
+        </div>
+        <div className="hidden md:flex grow">
+          <img src={"/login-wave.png"} className="rotate-180 h-screen -m-1" />
+          <div
+            className={`${titleFont.className} text-center text-white/70 w-[50vw] items-center justify-center flex bg-gradient-to-l to-[#3baee7] from-[#144f6d]`}
+          >
+            <p className="text-2xl rotate-90">
+              Conquer the depths of the ocean!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ResetPasswordPage;
+export default Reset;
