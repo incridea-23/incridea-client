3b94521a8837323432493682ed8d469fda91f2b2
Kumar Ankush <000.kumarankush@gmail.com>
2023-04-13 18:25:40 +0530

    add: contents

--- src/components/pages/about/AboutIncridea.tsx
+++ src/components/pages/about/AboutIncridea.tsx
@@ -1,57 +1,66 @@
-
-import Image from 'next/image';
-import { FC } from 'react';
-import { motion,useScroll,useTransform } from 'framer-motion';
+import Image from "next/image";
+import { FC } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const AboutIncridea: FC = () => {
   let { scrollYProgress } = useScroll();
-  let y = useTransform(scrollYProgress,[0,1],["0%","-100%"])
+  let y = useTransform(scrollYProgress, [0, 1], ["40%", "0%"]);
   return (
     <>
       <div className="relative h-96 w-full overflow-hidden">
-        <motion.div style={{y}} className='relative w-full h-full'>
-        <Image
-          src="/assets/png/incrediaAbout.png"
-          alt="Incridea Banner"
-          width={500}
-          height={500}
-          className="absolute inset-0 w-full h-full object-cover"
-        />
-        <span className="absolute inset-0 w-full h-full bg-gradient-to-b from-transparent to-black z-10"></span>
-        </motion.div>        
-          <div className="absolute inset-0 w-full h-full flex items-center justify-center">
-            <h1
-              data-scroll
-              data-scroll-speed="2"
-              className={`titleFont text-4xl lg:text-7xl text-center p-5 text-white`}
-            >
-              About Incridea
-            </h1>
-          </div>
+        <motion.div
+          style={{ y }}
+          className="relative h-full w-full overflow-hidden"
+        >
+          <Image
+            src="/assets/png/incrediaAbout.png"
+            alt="Incridea Banner"
+            width={500}
+            height={500}
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+          <span className="absolute inset-0 w-full h-full bg-gradient-to-b from-transparent to-black z-10"></span>
+        </motion.div>
+
+        <div className="absolute inset-0 w-full h-full flex items-center justify-center">
+          <h1
+            className={`titleFont text-4xl p-4 text-center lg:text-7xl text-white`}
+          >
+            About Incridea
+          </h1>
+        </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center gap-10 lg:gap-20 lg:p-10 p-5 basis-1/2 my-5">
+      <div className="flex flex-col lg:flex-row-reverse justify-center items-center gap-10 lg:gap-20 lg:p-10 p-5 basis-1/2">
         <Image
           src="/assets/png/logo-black.png"
           alt="Incridea Logo"
           width={500}
           height={500}
         />
-        <div className="text-white text-lg lg:text-xl text-justify basis-1/2">
-          Incridea, the annual college festival of NMAM Institute of Technology,
-          Nitte, has rapidly grown into one of the most awaited cultural events
-          in the region within a short span of time. The festival has become a
-          symbol of creativity, innovation, and fun, attracting students from
-          various colleges across the country. With an array of exciting
-          activities, including fashion shows, music and dance competitions,
-          drama competitions, sports competitions, coding competitions,
-          hackathons, robotics competitions, and workshops, Incridea provides a
-          platform for students to showcase their talents and enhance their
-          skills and knowledge. The festival also hosts renowned artists and
-          musicians, adding to the excitement and energy of the event. With its
-          growth and success, Incridea has become a testament to the spirit and
-          dedication of the NMAM Institute of Technology in promoting student
-          talent and providing a memorable experience for all who attend.
+
+        <div className="text-white text-lg lg:text-xl text-justify basis-1/2 space-y-[1em]">
+          <p>
+            Incridea, the annual college festival of NMAM Institute of
+            Technology, Nitte, has rapidly grown into one of the most awaited
+            cultural events in the region within a short span of time. The
+            festival has become a symbol of creativity, innovation, and fun,
+            attracting students from various colleges across the country.
+          </p>
+          <p>
+            Incridea is an entertainment hub that offers an array of 60+ events,
+            including pronites, fashion shows, music, drama, sports, robotics,
+            and coding competitions, with some being our flagship events. These
+            events attract several thousand attendees and provide a platform for
+            students to showcase their talents while connecting with peers from
+            all over the country.
+          </p>
+          <p>
+            Incridea&apos;s growth and success stand as a testament to
+            NMAMIT&apos;s commitment to promoting student talent and creating
+            unforgettable experiences for all attendees. As we move forward,
+            we&apos;re excited to embrace the Tides of Change! Are you?
+          </p>
         </div>
       </div>
     </>
