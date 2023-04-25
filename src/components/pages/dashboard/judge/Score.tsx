1760b16fc1563f10d354dbc18ab4dabc501a3186
swasthikshetty10 <swasthikshetty10902@gmail.com>
2023-04-26 02:48:37 +0530

    fix: time

--- src/components/pages/dashboard/judge/Score.tsx
+++ src/components/pages/dashboard/judge/Score.tsx
@@ -1,10 +1,10 @@
-import Spinner from '@/src/components/spinner';
-import createToast from '@/src/components/toast';
-import { AddScoreDocument, GetScoreDocument } from '@/src/generated/generated';
-import { formatTime, parseTime } from '@/src/utils/time';
-import { useMutation, useQuery } from '@apollo/client';
-import { useEffect, useState } from 'react';
-import { toast } from 'react-hot-toast';
+import Spinner from "@/src/components/spinner";
+import createToast from "@/src/components/toast";
+import { AddScoreDocument, GetScoreDocument } from "@/src/generated/generated";
+import { formatTime, parseTime } from "@/src/utils/time";
+import { useMutation, useQuery } from "@apollo/client";
+import { ChangeEvent, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 const Score = ({
   teamId,
@@ -17,7 +17,7 @@ const Score = ({
   roundNo: number;
   type: string;
 }) => {
-  const { data, loading } = useQuery(GetScoreDocument, {
+  const { data, loading, error } = useQuery(GetScoreDocument, {
     variables: {
       criteriaId: criteriaId,
       teamId: teamId,
@@ -26,27 +26,25 @@ const Score = ({
     skip: !roundNo || !teamId || !criteriaId,
   });
 
-  const [score, setScore] = useState<string>('0');
+  const [score, setScore] = useState<string>("0");
   // as soon as data is loaded, set score
   useEffect(() => {
-    if (data?.getScore?.__typename === 'QueryGetScoreSuccess') {
+    if (data?.getScore?.__typename === "QueryGetScoreSuccess") {
       setScore(data.getScore.data.score);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.getScore]);
 
-  const [updateScore, { loading: updateScoreLoading }] = useMutation(
-    AddScoreDocument,
-    {
-      refetchQueries: ['GetScore', 'GetTotalScores'],
+  const [updateScore, { loading: updateScoreLoading, error: updateError }] =
+    useMutation(AddScoreDocument, {
+      refetchQueries: ["GetScore", "GetTotalScores"],
       awaitRefetchQueries: true,
-    }
-  );
-
+    });
+  console.log(error, updateError);
   const handleUpdateScore = () => {
     // check if score is really changed before updating
     if (
-      data?.getScore.__typename === 'QueryGetScoreSuccess' &&
+      data?.getScore.__typename === "QueryGetScoreSuccess" &&
       data.getScore.data.score === score
     ) {
       return;
@@ -56,16 +54,16 @@ const Score = ({
         criteriaId: Number(criteriaId),
         teamId: Number(teamId),
         // if input is empty, set score to 0
-        score: score ? score : '0',
+        score: score ? score : "0",
       },
     }).then((res) => {
-      if (res.data?.addScore.__typename === 'Error') {
+      if (res.data?.addScore.__typename === "Error") {
         toast.error(res.data.addScore.message, {
-          position: 'bottom-center',
+          position: "bottom-center",
         });
       }
     });
-    createToast(promise, 'Updating score...');
+    createToast(promise, "Updating score...");
   };
 
   // Auto save score after 500ms
@@ -94,16 +92,15 @@ const Score = ({
     <div className="flex items-center text-lg gap-2">
       {loading ? (
         <Spinner />
-      ) : type === 'TIME' ? (
-        <TimeInput
-          disabled={loading || updateScoreLoading}
-          value={score}
+      ) : type === "TIME" ? (
+        <TimePicker
+          milliseconds={Number(score)}
           onChange={(newTime) => setScore(newTime.toString())}
         />
       ) : (
         <input
-          max={type === 'NUMBER' ? 10 : undefined}
-          min={type === 'NUMBER' ? 0 : undefined}
+          max={type === "NUMBER" ? 10 : undefined}
+          min={type === "NUMBER" ? 0 : undefined}
           value={score}
           onChange={(e) => setScore(e.target.value)}
           type={type.toLowerCase()}
@@ -118,36 +115,100 @@ const Score = ({
 
 export default Score;
 
-type Props = {
-  value: string;
-  onChange: (milliseconds: number) => void;
-  disabled?: boolean;
-};
+interface TimePickerProps {
+  milliseconds: number;
+  onChange: (newTime: number) => void;
+}
 
-function TimeInput({ value, onChange, disabled }: Props) {
-  const [inputValue, setInputValue] = useState(
-    value === '0' ? '00:00.000' : formatTime(Number(value))
-  );
+const TimePicker: React.FC<TimePickerProps> = ({ milliseconds, onChange }) => {
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [millis, setMillis] = useState<number>(0);
 
   useEffect(() => {
-    if (isNaN(Number(value))) return;
-    setInputValue(formatTime(Number(value)));
-  }, [value]);
+    setHours(Math.floor(milliseconds / 3600000));
+    setMinutes(Math.floor((milliseconds % 3600000) / 60000));
+    setSeconds(Math.floor(((milliseconds % 3600000) % 60000) / 1000));
+    setMillis(Math.floor(((milliseconds % 3600000) % 60000) % 1000));
+  }, [milliseconds]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    const milliseconds = parseTime(inputValue);
-    setInputValue(inputValue);
-    onChange(milliseconds);
+  const handleHoursChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setHours(value);
+    console.log(value, minutes, seconds, millis);
+    const timeInMs =
+      (isNaN(value) ? 0 : value) * 3600000 +
+      (isNaN(minutes) ? 0 : minutes) * 60000 +
+      (isNaN(seconds) ? 0 : seconds) * 1000 +
+      (isNaN(millis) ? 0 : millis);
+    console.log(timeInMs);
+    onChange(timeInMs);
+  };
+
+  const handleMinutesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setMinutes(value);
+    const timeInMs =
+      (isNaN(hours) ? 0 : hours) * 3600000 +
+      (isNaN(value) ? 0 : value) * 60000 +
+      (isNaN(seconds) ? 0 : seconds) * 1000 +
+      (isNaN(millis) ? 0 : millis);
+    onChange(timeInMs);
+  };
+
+  const handleSecondsChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setSeconds(value);
+    const timeInMs =
+      (isNaN(hours) ? 0 : hours) * 3600000 +
+      (isNaN(minutes) ? 0 : minutes) * 60000 +
+      (isNaN(value) ? 0 : value) * 1000 +
+      (isNaN(millis) ? 0 : millis);
+    onChange(timeInMs);
+  };
+
+  const handleMillisChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+    setMillis(value);
+    const timeInMs =
+      (isNaN(hours) ? 0 : hours) * 3600000 +
+      (isNaN(minutes) ? 0 : minutes) * 60000 +
+      (isNaN(seconds) ? 0 : seconds) * 1000 +
+      (isNaN(value) ? 0 : value);
+    onChange(timeInMs);
   };
 
   return (
-    <input
-      type="text"
-      value={inputValue}
-      onChange={handleInputChange}
-      placeholder="MM:SS.SSS"
-      className="bg-black/20 w-28 p-2 rounded-lg text-center text-white/90 focus:ring-2 ring-white/50 outline-none"
-    />
+    <div className="flex flex-col items-center space-x-4">
+      <label className="text-gray-100">Hours:</label>
+      <input
+        className="border bg-black/10 border-gray-400 rounded-md w-16 px-2 py-1"
+        type="number"
+        value={hours}
+        onChange={handleHoursChange}
+      />
+      <label className="text-gray-100">Min:</label>
+      <input
+        className="border bg-black/10 border-gray-400 rounded-md w-16 px-2 py-1"
+        type="number"
+        value={minutes}
+        onChange={handleMinutesChange}
+      />
+      <label className="text-gray-100">Sec:</label>
+      <input
+        className="border bg-black/10 border-gray-400 rounded-md w-16 px-2 py-1"
+        type="number"
+        value={seconds}
+        onChange={handleSecondsChange}
+      />
+      <label className="text-gray-100">Ms:</label>
+      <input
+        className="border bg-black/10 border-gray-400 rounded-md w-16 px-2 py-1"
+        type="number"
+        value={millis}
+        onChange={handleMillisChange}
+      />
+    </div>
   );
-}
+};
