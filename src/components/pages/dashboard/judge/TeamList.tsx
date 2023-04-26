b56933a3e718197816a05a99e47bd98b1b31ff7d
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-04-26 06:35:09 +0530

    fix: winner type

--- src/components/pages/dashboard/judge/TeamList.tsx
+++ src/components/pages/dashboard/judge/TeamList.tsx
@@ -1,6 +1,6 @@
-import Button from "@/src/components/button";
-import Spinner from "@/src/components/spinner";
-import createToast from "@/src/components/toast";
+import Button from '@/src/components/button';
+import Spinner from '@/src/components/spinner';
+import createToast from '@/src/components/toast';
 import {
   ChangeSelectStatusDocument,
   CreateWinnerDocument,
@@ -10,13 +10,13 @@ import {
   PromoteToNextRoundDocument,
   WinnerType,
   WinnersByEventQuery,
-} from "@/src/generated/generated";
-import { idToPid, idToTeamId } from "@/src/utils/id";
-import { useMutation, useQuery, useSubscription } from "@apollo/client";
-import React, { useEffect } from "react";
-import { toast } from "react-hot-toast";
-import { AiFillEye, AiOutlineSearch } from "react-icons/ai";
-import ViewTeamModal from "./ViewTeamModal";
+} from '@/src/generated/generated';
+import { idToPid, idToTeamId } from '@/src/utils/id';
+import { useMutation, useQuery, useSubscription } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { AiOutlineSearch } from 'react-icons/ai';
+import ViewTeamModal from './ViewTeamModal';
 
 type Props = {
   data: JudgeGetTeamsByRoundSubscription | undefined;
@@ -45,12 +45,12 @@ const TeamList = ({
   finalRound,
   winners,
 }: Props) => {
-  const [query, setQuery] = React.useState("");
-  const [sortOrder, setSortOrder] = React.useState<"asc" | "desc">("asc");
+  const [query, setQuery] = React.useState('');
+  const [sortOrder, setSortOrder] = React.useState<'asc' | 'desc'>('asc');
   const [sortField, setSortField] = React.useState<
-    "Total Score" | "Your Score"
-  >("Total Score");
-  const [winnerType, setWinnerType] = React.useState<WinnerType[0]>("WINNER");
+    'Total Score' | 'Your Score'
+  >('Total Score');
+  const [winnerType, setWinnerType] = React.useState<WinnerType[0]>('WINNER');
 
   const [promote, { loading: promoteLoading }] = useMutation(
     PromoteToNextRoundDocument
@@ -59,7 +59,7 @@ const TeamList = ({
   const [changeStatus, { loading: changeLoading }] = useMutation(
     ChangeSelectStatusDocument,
     {
-      refetchQueries: ["GetTotalScores"],
+      refetchQueries: ['GetTotalScores'],
       awaitRefetchQueries: true,
     }
   );
@@ -67,7 +67,7 @@ const TeamList = ({
   const [selectWinner, { loading: winnerLoading }] = useMutation(
     CreateWinnerDocument,
     {
-      refetchQueries: ["WinnersByEvent"],
+      refetchQueries: ['WinnersByEvent'],
       awaitRefetchQueries: true,
     }
   );
@@ -96,7 +96,7 @@ const TeamList = ({
   useEffect(() => {
     if (
       roundStatus?.getRoundStatus.__typename ===
-      "SubscriptionGetRoundStatusSuccess"
+      'SubscriptionGetRoundStatusSuccess'
     ) {
       if (roundStatus.getRoundStatus.data.selectStatus) {
         setSelectionMode(true);
@@ -110,9 +110,9 @@ const TeamList = ({
   }, [roundStatus]);
 
   const teamOrParticipant =
-    eventType === "INDIVIDUAL" || eventType === "INDIVIDUAL_MULTIPLE_ENTRY"
-      ? "Participant"
-      : "Team";
+    eventType === 'INDIVIDUAL' || eventType === 'INDIVIDUAL_MULTIPLE_ENTRY'
+      ? 'Participant'
+      : 'Team';
 
   const handlePromote = (teamId: string) => {
     const promise = promote({
@@ -122,16 +122,16 @@ const TeamList = ({
         selected: true,
       },
     });
-    createToast(promise, "Promoting team to next round...");
+    createToast(promise, 'Promoting team to next round...');
   };
 
-  const sorter = sortField === "Total Score" ? "totalScore" : "judgeScore";
+  const sorter = sortField === 'Total Score' ? 'totalScore' : 'judgeScore';
 
   const getTotalScore = (
-    team: JudgeGetTeamsByRoundSubscription["judgeGetTeamsByRound"][0]
+    team: JudgeGetTeamsByRoundSubscription['judgeGetTeamsByRound'][0]
   ) => {
     const score =
-      scores?.getTotalScores.__typename === "QueryGetTotalScoresSuccess"
+      scores?.getTotalScores.__typename === 'QueryGetTotalScoresSuccess'
         ? scores?.getTotalScores.data.find(
             (score) => score.teamId === Number(team.id)
           )?.[sorter]
@@ -143,10 +143,10 @@ const TeamList = ({
     (team) => {
       if (
         finalRound &&
-        winners?.winnersByEvent.__typename === "QueryWinnersByEventSuccess"
+        winners?.winnersByEvent.__typename === 'QueryWinnersByEventSuccess'
       ) {
         const isWinner =
-          winners?.winnersByEvent.__typename === "QueryWinnersByEventSuccess" &&
+          winners?.winnersByEvent.__typename === 'QueryWinnersByEventSuccess' &&
           winners?.winnersByEvent.data.find(
             (winner) => winner.team.id === team.id
           );
@@ -160,49 +160,55 @@ const TeamList = ({
   const sortedTeams = [...(filteredTeams ?? [])].sort((team1, team2) => {
     const score1 = getTotalScore(team1) ?? 0;
     const score2 = getTotalScore(team2) ?? 0;
-    if (sortOrder === "asc") {
+    if (sortOrder === 'asc') {
       return score1 - score2;
-    } else if (sortOrder === "desc") {
+    } else if (sortOrder === 'desc') {
       return score2 - score1;
     } else {
       return 0;
     }
   });
 
+  enum WinnersType {
+    Winner = 'WINNER',
+    RunnerUp = 'RUNNER_UP',
+    SecondRunnerUp = 'SECOND_RUNNER_UP',
+  }
+
   return (
     <div className="h-full overflow-y-auto">
       <div className="p-3 shadow-sm mb-1 rounded-t-lg top-0 sticky bg-[#35436F] flex justify-between">
         <div className="relative w-full mr-5">
           <input
-            type={"text"}
+            type={'text'}
             placeholder="Search by name or PID"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             className={`bg-white/20 w-full  h-10 px-4 pr-16 rounded-lg text-sm placeholder:text-white/60 focus:outline-none focus:ring-2 ring-white/40`}
           />
           <AiOutlineSearch
-            size={"1.4rem"}
+            size={'1.4rem'}
             className="absolute right-3 top-2.5 text-white/60"
           />
         </div>
         {selectionMode && (
           <>
             <Button
-              size={"small"}
-              intent={"dark"}
+              size={'small'}
+              intent={'dark'}
               className="mr-2"
-              onClick={() =>
-                setSortOrder(sortOrder === "asc" ? "desc" : "asc")
-              }>
-              {sortOrder === "asc" ? "▲" : "▼"}
+              onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}
+            >
+              {sortOrder === 'asc' ? '▲' : '▼'}
             </Button>
 
             <select
               value={sortField}
               onChange={(e) =>
-                setSortField(e.target.value as "Total Score" | "Your Score")
+                setSortField(e.target.value as 'Total Score' | 'Your Score')
               }
-              className="bg-white/20 text-white/60 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 ring-white/40 mr-2">
+              className="bg-white/20 text-white/60 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 ring-white/40 mr-2"
+            >
               <option value="Total Score">Total Score</option>
               <option value="Your Score">Your Score</option>
             </select>
@@ -217,33 +223,35 @@ const TeamList = ({
                 roundNo,
               },
             }).then((data) => {
-              if (data.data?.changeSelectStatus.__typename === "Error") {
+              if (data.data?.changeSelectStatus.__typename === 'Error') {
                 toast.error(data.data.changeSelectStatus.message, {
-                  position: "bottom-center",
+                  position: 'bottom-center',
                 });
               }
             });
             setSelectionMode(!selectionMode);
           }}
           disabled={changeLoading}
-          intent={"success"}
+          intent={'success'}
           className="w-40"
-          noScaleOnHover>
-          {selectionMode ? "Go Back" : "Select"}
+          noScaleOnHover
+        >
+          {selectionMode ? 'Go Back' : 'Select'}
         </Button>
       </div>
 
       <div className="flex px-3 pb-3 flex-col gap-2 mt-3">
         {selectionMode && finalRound && (
-          <div className="flex flex-row-reverse justify-between my-3">
-            {Object.values(WinnerType).map((type) => (
+          <div className="flex flex-row justify-between my-3">
+            {Object.values(WinnersType).map((type) => (
               <Button
                 key={type}
                 onClick={() => setWinnerType(type)}
-                intent={type === winnerType ? "primary" : "ghost"}
+                intent={type === winnerType ? 'primary' : 'ghost'}
                 className="mr-2"
-                noScaleOnHover>
-                {type.replaceAll("_", " ")}
+                noScaleOnHover
+              >
+                {type.replaceAll('_', ' ')}
               </Button>
             ))}
           </div>
@@ -254,8 +262,9 @@ const TeamList = ({
             <div className={`basis-4/12 text-white/80`}>ID</div>
             <div
               className={`${
-                selectionMode ? "basis-1/12" : "basis-4/12"
-              } text-white/80`}>
+                selectionMode ? 'basis-1/12' : 'basis-4/12'
+              } text-white/80`}
+            >
               Score
             </div>
             {selectionMode && (
@@ -263,11 +272,11 @@ const TeamList = ({
             )}
             {selectionMode ? (
               <div className={`basis-2/12 text-white/80`}>
-                {finalRound ? "Select" : "Promote"}
+                {finalRound ? 'Select' : 'Promote'}
               </div>
             ) : (
               <div className={`basis-2/12 flex justify-end items-end`}>
-                {teamOrParticipant === "Team" && "View"}
+                {teamOrParticipant === 'Team' && 'View'}
               </div>
             )}
           </div>
@@ -296,39 +305,43 @@ const TeamList = ({
               }}
               className={`flex items-center p-2 px-5 bg-white/10 rounded-lg cursor-pointer ${
                 selectedTeam === team?.id
-                  ? "bg-white/50"
-                  : "hover:bg-white/20 transition-colors duration-300"
-              }`}>
+                  ? 'bg-white/50'
+                  : 'hover:bg-white/20 transition-colors duration-300'
+              }`}
+            >
               <div className="flex flex-row gap-5 w-full">
                 <div
                   className={`basis-4/12 ${
                     selectedTeam === team?.id
-                      ? "text-black/80"
-                      : "text-white/80"
-                  }`}>
+                      ? 'text-black/80'
+                      : 'text-white/80'
+                  }`}
+                >
                   {team?.name}
                 </div>
 
                 <div
                   className={`basis-4/12 ${
                     selectedTeam === team?.id
-                      ? "text-black/60"
-                      : "text-white/60"
-                  }`}>
-                  {teamOrParticipant === "Team"
+                      ? 'text-black/60'
+                      : 'text-white/60'
+                  }`}
+                >
+                  {teamOrParticipant === 'Team'
                     ? idToTeamId(team?.id!)
                     : idToPid(team.leaderId?.toString()!)}
                 </div>
 
                 <div
-                  className={`${selectionMode ? "basis-1/12" : "basis-4/12"} ${
+                  className={`${selectionMode ? 'basis-1/12' : 'basis-4/12'} ${
                     selectedTeam === team?.id
-                      ? "text-black/60"
-                      : "text-white/60"
-                  }`}>
+                      ? 'text-black/60'
+                      : 'text-white/60'
+                  }`}
+                >
                   {scoresLoading && <Spinner />}
                   {scores?.getTotalScores.__typename ===
-                    "QueryGetTotalScoresSuccess" &&
+                    'QueryGetTotalScoresSuccess' &&
                     scores?.getTotalScores.data.find(
                       (score) => score.teamId === Number(team?.id)
                     )?.judgeScore}
@@ -338,12 +351,13 @@ const TeamList = ({
                   <div
                     className={`basis-1/12 ${
                       selectedTeam === team?.id
-                        ? "text-black/60"
-                        : "text-white/60"
-                    }`}>
+                        ? 'text-black/60'
+                        : 'text-white/60'
+                    }`}
+                  >
                     {scoresLoading && <Spinner />}
                     {scores?.getTotalScores.__typename ===
-                      "QueryGetTotalScoresSuccess" &&
+                      'QueryGetTotalScoresSuccess' &&
                       scores?.getTotalScores.data.find(
                         (score) => score.teamId === Number(team?.id)
                       )?.totalScore}
@@ -352,8 +366,9 @@ const TeamList = ({
 
                 <div
                   className={`basis-2/12 ${
-                    !selectionMode ? "flex justify-end items-end" : ""
-                  }`}>
+                    !selectionMode ? 'flex justify-end items-end' : ''
+                  }`}
+                >
                   {selectionMode && !finalRound && (
                     <input
                       disabled={promoteLoading}
@@ -362,7 +377,7 @@ const TeamList = ({
                       onChange={() => handlePromote(team?.id!)}
                     />
                   )}
-                  {!selectionMode && teamOrParticipant === "Team" && (
+                  {!selectionMode && teamOrParticipant === 'Team' && (
                     <ViewTeamModal
                       teamName={team.name}
                       members={team.members}
@@ -371,7 +386,7 @@ const TeamList = ({
                   {selectionMode && finalRound && (
                     <Button
                       disabled={winnerLoading}
-                      size={"small"}
+                      size={'small'}
                       onClick={() => {
                         let promise = selectWinner({
                           variables: {
@@ -380,14 +395,15 @@ const TeamList = ({
                             type: winnerType as WinnerType,
                           },
                         }).then((data) => {
-                          if (data.data?.createWinner.__typename === "Error") {
-                            toast.error("Error occured!!", {
-                              position: "bottom-center",
+                          if (data.data?.createWinner.__typename === 'Error') {
+                            toast.error('Error occured!!', {
+                              position: 'bottom-center',
                             });
                           }
                         });
                         createToast(promise, `Selecting ${winnerType}...`);
-                      }}>
+                      }}
+                    >
                       +
                     </Button>
                   )}
