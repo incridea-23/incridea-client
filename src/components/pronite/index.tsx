3793560ff2b07968eee5a67edb21ed212b519a90
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-04-28 09:13:05 +0530

    fix: already registered

--- src/components/pronite/index.tsx
+++ src/components/pronite/index.tsx
@@ -1,9 +1,12 @@
-import { RegisterProniteDocument } from "@/src/generated/generated";
-import { pidToId } from "@/src/utils/id";
-import { useMutation } from "@apollo/client";
-import React, { useEffect } from "react";
-import Button from "../button";
-import Spinner from "../spinner";
+import {
+  RegisterProniteDocument,
+  UserByIdDocument,
+} from '@/src/generated/generated';
+import { pidToId } from '@/src/utils/id';
+import { useMutation, useQuery } from '@apollo/client';
+import Button from '../button';
+import Spinner from '../spinner';
+import { useEffect } from 'react';
 
 function Pronite({ pId }: { pId: string }) {
   const [registerPronite, { data, loading, error }] = useMutation(
@@ -15,35 +18,75 @@ function Pronite({ pId }: { pId: string }) {
     }
   );
 
+  const {
+    data: userData,
+    loading: userLoading,
+    error: userError,
+  } = useQuery(UserByIdDocument, {
+    variables: {
+      id: pidToId(pId),
+    },
+  });
+
   return (
     <>
       <div className="max-w-sm mt-1 mb-3">
         <Button
-          intent={"success"}
+          intent={'success'}
           className="bg-blue-500 mx-auto hover:bg-blue-700 text-white font-bold py-2.5 px-7 rounded"
-          onClick={() => registerPronite()}>
+          onClick={() => registerPronite()}
+        >
           Register
         </Button>
       </div>
       {loading ? (
         <>
-          <Spinner className="mt-3" intent={"white"} size={'small'} />
+          <Spinner className="mt-3" intent={'white'} size={'small'} />
         </>
-      ) : data?.registerPronite.__typename === "MutationRegisterProniteSuccess" ? (
+      ) : data?.registerPronite.__typename ===
+        'MutationRegisterProniteSuccess' ? (
         <div className="p-3 bg-white/10 rounded-md bodyFont">
           <div className="text-lg leading-snug mb-1 text-green-500">
-            <span className="font-bold">{pId}</span> registered for pronite
+            <span className="font-bold">{pId}</span> registered for Pronite
           </div>
           <div className="text-white">
-            <div className="text-lg leading-snug">{data.registerPronite.data.user.name}</div>
-            {/* <div className="text-xs">{data.registerPronite.data.user.email}</div> */}
-            <div className="text-sm leading-snug">{data.registerPronite.data.user.college?.name}</div>
-            <div className="text-sm leading-snug">{data.registerPronite.data.user.phoneNumber}</div>
+            <div className="text-lg leading-snug">
+              {data.registerPronite.data.user.name}
+            </div>
+            <div className="text-sm leading-snug">
+              {data.registerPronite.data.user.college?.name}
+            </div>
+            <div className="text-sm leading-snug">
+              {data.registerPronite.data.user.phoneNumber}
+            </div>
           </div>
         </div>
       ) : (
         <div className="text-red-500 font-semibold bodyFont bg-white/10 rounded-md">
-          {data?.registerPronite.message && <p className="p-3 py-2">{data.registerPronite.message}</p>}
+          {userLoading && <Spinner intent={'white'} size={'small'} />}
+          {data?.registerPronite.message && (
+            <div>
+              <p className="p-3 py-2">{data.registerPronite.message}</p>
+              {userData?.userById.__typename === 'QueryUserByIdSuccess' && (
+                <div className="p-3 bg-white/10 rounded-md bodyFont">
+                  <div className="text-lg leading-snug mb-1">
+                    <span className="font-bold text-green-500">{pId}</span>
+                  </div>
+                  <div className="text-white">
+                    <div className="text-lg leading-snug">
+                      {userData.userById.data.name}
+                    </div>
+                    <div className="text-sm leading-snug">
+                      {userData.userById.data.college?.name}
+                    </div>
+                    <div className="text-sm leading-snug">
+                      {userData.userById.data.phoneNumber}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       )}
     </>
