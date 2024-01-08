712bd3c7a19c8ce7cf01a744fb8453baf177471f
swasthikshetty10 <swasthikshetty10902@gmail.com>
2024-01-08 21:10:22 +0530

    feat: redirect

--- src/pages/theme.tsx
+++ src/pages/theme.tsx
@@ -1,75 +1,25 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
+import Loader from "../components/Loader";
 
 function Theme() {
-  const config = {
-    angle: 290,
-    spread: 360,
-    startVelocity: 40,
-    elementCount: 150,
-    dragFriction: 0.11,
-    duration: 5020,
-    stagger: 3,
-    width: "8px",
-    height: "14px",
-    perspective: "503px",
-    colors: [
-      "#f00",
-      "#0f0",
-      "#00f",
-      "#FFC700",
-      "#FF0000",
-      "#2E3191",
-      "#41BBC7",
-    ],
-  };
-  const [show, setShow] = useState(false);
+  // redirect to youtube
+  const router = useRouter();
   useEffect(() => {
-    ConfettiExplosion();
-  }, [setShow]);
-  const ConfettiExplosion = () => {
-    setShow(true);
-    setTimeout(() => setShow(false), 4000);
-  };
+    router.push("https://youtu.be/GdmrDe-VIlo?feature=shared");
+  }, [router]);
+
   return (
-    <div className="h-screen w-screen bg-white p-5 overflow-hidden  justify-center items-center flex flex-col relative">
-      <div className="absolute top-0 left-0 z-20">
-        <Confetti active={show} config={config} />
-      </div>
+    <div className="h-screen w-screen bg-black flex justify-center items-center">
       <Image
-        src={"/assets/gif/rick-roll.gif"}
-        alt="loader"
-        width={500}
-        height={500}
-        priority
-        className="absolute top-0 left-0 h-screen w-screen object-cover object-center "
+        src={"/assets/png/logo.png"}
+        className="animate-pulse"
+        alt="logo"
+        width={300}
+        height={300}
       />
-      <div className="z-10 text-center space-y-5">
-        <h1
-          className=" text-7xl text-center font-bold  "
-          style={{
-            WebkitTextStroke: "1px #FFF",
-          }}>
-          Incridea Theme
-        </h1>
-        <p
-          className="text-black  text-2xl font-semibold "
-          style={{
-            WebkitTextStroke: "0.5px #FFF",
-          }}>
-          Checkout our{" "}
-          <a
-            style={{
-              WebkitTextStroke: "0.5px #FFF",
-            }}
-            href="https://www.instagram.com/incridea/"
-            className="italic underline">
-            instagram
-          </a>{" "}
-          page for more updates
-        </p>
-      </div>
     </div>
   );
 }
