911324d4d4709e5119f045fe9fe063f2c7aa0cbd
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-01-21 23:48:00 +0530

    feat: added responsiveness and ground

--- src/components/exploreGame/index.tsx
+++ src/components/exploreGame/index.tsx
@@ -2,7 +2,11 @@ import useWindowSize from "@/src/hooks/useWindowSize";
 import { useRef, useEffect, useState } from "react";
 export default function ExploreGame() {
   const canvas = useRef<HTMLCanvasElement>(null);
-  const WINDOW_DIMENSION = useWindowSize();
+  // const WINDOW_DIMENSION = useWindowSize();
+  const WINDOW_DIMENSION = {
+    width: 0,
+    height: 0,
+  };
   const [direction, setDirection] = useState<"left" | "right">("right");
   const walkSprite = [
     {
@@ -80,22 +84,70 @@ export default function ExploreGame() {
     background: HTMLImageElement
   ) {
     if (ctx) {
-      const imgHeight = (WINDOW_DIMENSION.height as number) * 2;
-      const imgWidth = ((imgHeight as number) * 2) / 7;
-      const repeatCount = Math.ceil(
-        (WINDOW_DIMENSION.width && WINDOW_DIMENSION.width / imgWidth) as number
-      );
+      const imgHeight = WINDOW_DIMENSION.height * 2;
+      const imgWidth = Math.ceil((imgHeight * 2) / 7); // 1000 is the width of the background image, 3500 is the height of the background image
+      const repeatCount = Math.ceil(WINDOW_DIMENSION.width / imgWidth);
       for (let i = 0; i < repeatCount; i++) {
-        ctx.drawImage(
-          background,
-          i * imgWidth,
-          0,
-          imgWidth,
-          imgHeight as number
-        );
+        ctx.drawImage(background, i * imgWidth, 0, imgWidth, imgHeight);
       }
     }
   }
+
+  const drawPlatform = (
+    ctx: CanvasRenderingContext2D | null | undefined,
+    platformSprite: HTMLImageElement
+  ) => {
+    if (canvas.current && ctx) {
+      // Drawing the ground
+      const groundSpriteHeight = WINDOW_DIMENSION.height * 0.3;
+      const groundSpriteWidth = Math.ceil((groundSpriteHeight * 500) / 245); // 500 is the width of the platform sprite, 245 is the height of the platform sprite
+      const repeatCountGround = Math.ceil(
+        WINDOW_DIMENSION.width / groundSpriteWidth
+      );
+      for (let i = 0; i < repeatCountGround; i++) {
+        ctx.drawImage(
+          platformSprite,
+          15,
+          475,
+          500,
+          245,
+          i * groundSpriteWidth,
+          WINDOW_DIMENSION.height * 1.7,
+          groundSpriteWidth,
+          groundSpriteHeight
+        );
+      }
+
+      // Drawing the grass
+      const grassSpriteHeight = Math.ceil(WINDOW_DIMENSION.height * 0.123);
+      const grassSpriteWidth = grassSpriteHeight * 5; // 500 is the width of the background image, 100 is the height of the background image
+      const repeatCountGrass = Math.ceil(
+        WINDOW_DIMENSION.width / grassSpriteWidth
+      );
+      for (let i = 0; i < repeatCountGrass; i++) {
+        ctx.drawImage(
+          platformSprite,
+          15,
+          355,
+          500,
+          100,
+          i * grassSpriteWidth,
+          WINDOW_DIMENSION.height * 1.577,
+          grassSpriteWidth,
+          grassSpriteHeight
+        );
+      }
+    }
+  };
+
+  const canvasResize = () => {
+    if (canvas.current) {
+      WINDOW_DIMENSION.width = window.innerWidth;
+      WINDOW_DIMENSION.height = window.innerHeight;
+      canvas.current.width = WINDOW_DIMENSION.width;
+      canvas.current.height = 2 * WINDOW_DIMENSION.height;
+    }
+  };
 
   useEffect(() => {
     const ctx = canvas.current?.getContext("2d");
@@ -104,20 +156,18 @@ export default function ExploreGame() {
     const background = new Image();
     background.src = "/assets/spriteSheets/background.png";
     const platformSprite = new Image();
-    platformSprite.src = "/assets/spriteSheets/platformSpite.png";
+    platformSprite.src = "/assets/spriteSheets/platformSprite.png";
     if (canvas.current) {
-      canvas.current.width = WINDOW_DIMENSION.width as number;
-      canvas.current.height = 2 * (WINDOW_DIMENSION.height as number);
-      // console.log(WINDOW_DIMENSION.width, WINDOW_DIMENSION.height);
+      canvasResize();
+
+      window.addEventListener("resize", () => {
+        canvasResize();
+      });
     }
     function animate() {
-      ctx?.clearRect(
-        0,
-        0,
-        WINDOW_DIMENSION.width as number,
-        (WINDOW_DIMENSION.height as number) * 2
-      );
+      ctx?.clearRect(0, 0, WINDOW_DIMENSION.width, WINDOW_DIMENSION.height * 2);
       drawBackground(ctx, background);
+      drawPlatform(ctx, platformSprite);
       ctx?.drawImage(
         spriteSheet,
         walkSprite[spriteIndex].x,
@@ -141,15 +191,20 @@ export default function ExploreGame() {
       requestAnimationFrame(animate);
     }
     animate();
+
+    // const canvasRefCopy = canvas.current;
+    // return () => {
+    //   canvasRefCopy?.removeEventListener("resize", () => {});
+    // };
   }, []);
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <canvas ref={canvas} className="h-[200vh] w-full"></canvas>
-      <div className="flex w-[500px] justify-between">
+      {/* <div className="flex w-[500px] justify-between">
         <button onClick={MoveLeft}>Left</button>
         <button onClick={MoveRight}>Right</button>
-      </div>
+      </div> */}
     </div>
   );
 }
