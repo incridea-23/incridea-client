c2e93f5dd305633f7cb93a63c25fbc6051c6c1c3
AaronNazareth <jevil257@gmail.com>
2024-01-26 14:48:58 +0530

    feat: leaderboard made like coc

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -2,8 +2,9 @@ import Spinner from "@/src/components/spinner";
 import { GetXpLeaderboardDocument } from "@/src/generated/generated";
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
-import { User,Level,QueryGetXpLeaderboardSuccess,Xp } from "@/src/generated/generated";
 import { idToPid } from "@/src/utils/id";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 
 const leaderBoard = () => {
@@ -11,6 +12,7 @@ const leaderBoard = () => {
         [userId: string]: {
             levelPoints: number;
             name: string;
+            count: number;
         }
       }
     const{
@@ -26,6 +28,7 @@ const leaderBoard = () => {
         levelPoints: number;
         name: string;
         userId: string;
+        count: number;
     }[]>([]);
 
     useEffect(() => {
@@ -36,16 +39,19 @@ const leaderBoard = () => {
             const userId: string = item.user.id;
             const levelPoints: number = item.level.point;
             const userName: string = item.user.name;
+            const levelCount: number = 1;
           
             // Check if the user ID is already in the userTotalPoints object
             if (userTotalPoints[userId]) {
               // If yes, add the level points to the existing total
                 userTotalPoints[userId].levelPoints += levelPoints;
+                userTotalPoints[userId].count += levelCount;
             } else {
               // If no, create a new entry for the user ID
                 userTotalPoints[userId] = {
                     levelPoints,
-                    name: userName
+                    name: userName,
+                    count: 1
                 };
             }
           });
@@ -59,20 +65,36 @@ const leaderBoard = () => {
         userTotalPointsArray.sort((a, b) => b.levelPoints - a.levelPoints);
   
         // Limit to the top 100 entries
-        const top100Users = userTotalPointsArray.slice(0, 100);
-        console.log(top100Users)
-                setSortedLeaderboard(userTotalPointsArray);
+        const top15Users = userTotalPointsArray.slice(0, 15);
+                setSortedLeaderboard(top15Users);
             }
           
     }, [Leaderboard]);
 
+    const getColor = (i: number) => {
+        if(i === 1){
+            return "bg-gradient-to-b from-amber-200 to-yellow-500"
+        }else if(i === 2){
+            return "bg-gradient-to-b from-slate-300 to-slate-500"
+        }
+        else if(i === 3){
+            return "bg-gradient-to-b from-amber-700 to-amber-900"
+        }
+        else{
+            return "bg-gradient-to-b from-blue-200 to-cyan-200"
+        }
+    }
+    let isMobile = typeof window !== "undefined" && window.innerWidth < 768;
     return (
         <div className="overflow-x-hidden" style={{ willChange: "transform" }}>
-            <div className=" bg-gradient-to-bl from-black to-slate-900 min-h-screen relative pt-32">
+            <div className=" bg-gradient-to-bl bg-white min-h-screen relative pt-32">
                 <h1 className="text-white glitch text-4xl md:text-5xl text-center font-bold">
                     XP Leaderboard
                 </h1>
-                <div className="flex md:mx-36 mx-5 mt-10 md:mt-7 bg-white text-gray-300 bg-opacity-20 backdrop-filter backdrop-blur-lg bg-clip-padding rounded-t-lg p-1 items-center justify-evenly text-sm md:text-lg font-bold h-20">
+                <h3 className="my-6 text-white text-2xl text-center">
+                    Embark on an XP Quest: Uncover Hidden Easter Eggs and Level Up Your Experience!
+                </h3>
+                <div className="flex mb-2 md:mx-36 mx-5 mt-10 md:mt-7 rounded-lg bg-white text-gray-300 bg-opacity-20 backdrop-filter backdrop-blur-lg bg-clip-padding rounded-t-lg p-1 items-center justify-evenly text-sm md:text-xl font-bold h-20">
                     <h1 className="basis-1/4 text-center">Position</h1>
                     <h1 className="basis-1/4 text-center">Player Id</h1>
                     <h1 className="basis-1/4 text-center">Player Name</h1>
@@ -83,14 +105,42 @@ const leaderBoard = () => {
                     <Spinner className='text-gray-300' />
                 </div>
                 )}
-                <div className='md:mx-36 mx-5 text-white text-center'>
+                <div className='md:mx-36 mx-5 text-white text-center flex flex-col gap-2'>
                     {sortedLeaderboard.map((user,i) => (
                         <div key={user.userId}
-                        className={`bg-white/10 md:rounded-none rounded-lg flex flex-row items-center justify-center h-16 border-b-[1px] border-b-red-50 `}>
-                            <h1 className="basis-1/4 flex justify-center text-center text-sm md:text-lg">{i+1}</h1>
-                            <h1 className="basis-1/4 flex justify-center text-center text-sm md:text-lg">{idToPid(user.userId)}</h1>
-                            <h1 className="basis-1/4 flex justify-center text-center text-sm md:text-lg">{user.name}</h1>
-                            <h1 className="basis-1/4 flex justify-center text-center text-sm md:text-lg">{user.levelPoints}</h1>
+                        className={`${getColor(i+1)} shadow-2xl rounded-lg flex flex-row items-center justify-center h-16 `}>
+                            <h1 className="basis-1/4 flex gap-1 justify-center items-center text-center text-base md:text-xl">
+                                {i+1}.
+                                <Image
+                                    src={i+1 === 1 ? `/assets/png/level3.png` : i+1 === 2 ? `/assets/png/level2.png` : i+1 === 3 ? `/assets/png/level1.png` : "/assets/png/level4.png"}
+                                    width={isMobile ? 25 : 50}
+                                    height={isMobile ? 25 : 50}
+                                    alt="medal"
+                                    className="text-5xl bg-transparent z-30 w-14 md:w-auto"
+                                />
+                                {/* <div className="realtive grow">
+                                    <Image
+                                        src={`/assets/png/XP.webp`}
+                                        width={isMobile ? 15 : 35}
+                                        height={isMobile ? 15 : 35}
+                                        alt="medal"
+                                        className="text-5xl bg-transparent z-30 w-14 md:w-auto"
+                                    />
+                                    <span className="absolute">{user.count}</span>
+                                </div> */}
+                            </h1>
+                            <h1 className="basis-1/4 flex justify-center text-center text-base md:text-xl font-semibold">{idToPid(user.userId)}</h1>
+                            <h1 className="basis-1/4 flex justify-center text-center text-base md:text-xl font-semibold capitalize">{user.name}</h1>
+                            <h1 className="basis-1/4 flex flex-row justify-center items-center text-center font-semibold text-base md:text-xl">
+                                {user.levelPoints}
+                                <Image
+                                    src={`/assets/png/trophy.png`}
+                                    width={isMobile ? 25 : 50}
+                                    height={isMobile ? 25 : 50}
+                                    alt="medal"
+                                    className="text-5xl bg-transparent w-14 md:w-auto"
+                                />
+                            </h1>
                         </div>
                     ))}
                 </div>
