8aef18dba7ac606e5df949371d5267629ed6a115
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-01-31 20:43:51 +0530

    fix: final font fix

--- src/components/galleryslide/devices/final-component.tsx
+++ src/components/galleryslide/devices/final-component.tsx
@@ -4,11 +4,13 @@ import React from "react";
 const FinalComponent = () => {
   return (
     <div
-      className="w-full text-white flex flex-col items-center"
+      className="w-full text-white flex flex-col items-center font-VikingHell"
       id="animation"
     >
-      <h1 className="font-bold text-4xl md:text-6xl">Incridea 24</h1>;
-      <p className="text-sm md:text-lg">Its your time to make some great memories!</p>
+      <h1 className="font-bold text-4xl md:text-6xl tracking-widest">Incridea 24`</h1>;
+      <p className="text-sm md:text-lg tracking-wider">
+        Its your time to create great memories
+      </p>
       <Image
         src={"/assets/png/4.png"}
         alt="dice"
