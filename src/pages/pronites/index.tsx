1e60b027788b5bbaa2c5bfedab99e6db0508e38b
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2024-02-01 22:30:07 +0530

    feat: added pronite page

--- src/pages/pronites/index.tsx
+++ src/pages/pronites/index.tsx
@@ -1,86 +1,111 @@
-import { NextPage } from 'next';
-import React from 'react';
-import Image from 'next/image';
-import ProniteFooter from '../../components/proniteFooter';
-
+import * as THREE from "three";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
-  useHorizontalScroll,
-  useHorizontalTouch,
-} from '../../hooks/useHorizontal';
-import CharacterAnimation from '@/src/components/animation/character';
-import TextAnimation from '@/src/components/animation/text';
+  Reflector,
+  Text,
+  useTexture,
+  useGLTF,
+  MeshReflectorMaterial,
+} from "@react-three/drei";
 
-const Pronite: NextPage = () => {
-  const scrollRef = useHorizontalScroll();
-  useHorizontalTouch(scrollRef);
+export default function App() {
   return (
-    <>
-      <div className="relative h-screen bg-gradient-to-bl   overflow-y-hidden  from-indigo-200 via-sky-500 to-cyan-100">
-        {/* Beach bg */}
-        <Image src="/assets/svg/beach.svg" className='object-cover object-right-bottom' fill alt="beach" />
-
-        {/* Bushes bg */}
-        <div className="fixed z-10 pointer-events-none bottom-0 h-screen w-screen bg-bottom bg-fit bg-repeat-x bg-[url('/assets/svg/bushes1.svg')]" />
-        <div
-          ref={scrollRef}
-          className="absolute top-0 snap-start snap-x snap-mandatory flex overflow-x-auto h-screen w-screen min-w-screen mx:auto overflow-y-hidden text-gray-100"
-        >
-          <div className="snap-start min-w-full h-screen place-items-center w-screen">
-            <div className="absolute flex pointer-events-none flex-col w-full py-10 md:py-2 md:px-8 top-[35%]">
-              <CharacterAnimation
-                text="PRONITES"
-                className={`titleFont flex justify-center`}
-                textStyle="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)] text-4xl text-center sm:text-6xl"
-              />
-              <TextAnimation
-                text="Ride the waves into a starlit night"
-                className={`titleFont flex justify-center flex-wrap`}
-                textStyle="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.5)] text-2xl text-center sm:text-4xl"
-              />
-            </div>
-          </div>
-          <div className="snap-start min-w-full h-screen place-items-center w-screen">
-            <Image
-              className="relative h-screen w-screen -bottom-10 sm:bottom-0"
-              src="/assets/svg/setiaA.svg"
-              width={1000}
-              height={1000}
-              alt="setia"
-            />
-          </div>
-          <div className=" snap-start min-w-full h-screen place-items-center w-screen">
-            <Image
-              className="relative h-screen w-screen -bottom-10 sm:bottom-0"
-              src="/assets/svg/raghavA.svg"
-              width={1000}
-              height={1000}
-              alt="raghav"
-            />
-          </div>
-          <div className="snap-start min-w-full h-screen place-items-center w-screen">
-            <Image
-              className="relative h-screen w-screen -bottom-10 sm:bottom-0"
-              src="/assets/svg/proBroA.svg"
-              width={100}
-              height={1000}
-              alt="proBro"
-            />
-          </div>
-          <div className="snap-start min-w-full h-screen place-items-center w-screen">
-            <Image
-              className="h-screen w-screen"
-              src="/assets/svg/iboard.svg"
-              width={1000}
-              height={1000}
-              alt="rules"
-            />
-          </div>
-          <div className="snap-start min-w-full h-screen place-items-center w-screen">
-            <ProniteFooter className="relative h-screen w-screen sm:bottom-0" />
-          </div>
-        </div>
-      </div>
-    </>
+    <Canvas
+      style={{ height: "100vh", width: "100vw" }}
+      gl={{ alpha: false }}
+      camera={{ position: [0, 3, 100], fov: 15 }}
+    >
+      <color attach="background" args={["black"]} />
+      <fog attach="fog" args={["black", 15, 20]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          <Carla
+            rotation={[0, Math.PI - 0.4, 0]}
+            position={[-1.2, 0, 0.6]}
+            scale={[0.26, 0.26, 0.26]}
+          />
+          <VideoText position={[0, 1, -1]} />
+          <Ground />
+        </group>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[0, 10, 0]} intensity={1} />
+        <directionalLight position={[-50, 0, -40]} intensity={5} />
+        <Intro />
+      </Suspense>
+    </Canvas>
   );
-};
-export default Pronite;
+}
+
+function Carla(props: {
+  scale: number[];
+  position: number[];
+  rotation: number[];
+}) {
+  const { scene } = useGLTF("/assets/pronite/carla-draco.glb");
+  return <primitive object={scene} {...props} />;
+}
+
+function VideoText(props: { position: [x: number, y: number, z: number] }) {
+  const [video] = useState(() =>
+    Object.assign(document.createElement("video"), {
+      src: "/assets/pronite/textBg.mp4",
+      crossOrigin: "Anonymous",
+      loop: true,
+      muted: true,
+    })
+  );
+  useEffect(() => void video.play(), [video]);
+  return (
+    <Text
+      font="/font/Inter-Bold.woff"
+      fontSize={2}
+      letterSpacing={-0.06}
+      {...props}
+    >
+      Dhvani
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture
+          attach="map"
+          args={[video]}
+          colorSpace={THREE.SRGBColorSpace}
+        />
+      </meshBasicMaterial>
+    </Text>
+  );
+}
+
+function Ground() {
+  const [floor, normal] = useTexture([
+    "/assets/pronite/SurfaceImperfections003_1K_var1.jpg",
+    "/assets/pronite/SurfaceImperfections003_1K_Normal.jpg",
+  ]);
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+      <planeGeometry args={[20, 20]} />
+      <MeshReflectorMaterial
+        blur={[400, 100]}
+        resolution={512}
+        mirror={0.5}
+        mixBlur={6}
+        mixStrength={1.5}
+        color="#a0a0a0"
+        metalness={0.4}
+        roughnessMap={floor}
+        normalMap={normal}
+        normalScale={new THREE.Vector2(2, 2)}
+      />
+    </mesh>
+  );
+}
+
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3());
+  return useFrame((state) => {
+    state.camera.position.lerp(
+      vec.set(state.pointer.x * 5, 3 + state.pointer.y * 2, 14),
+      0.05
+    );
+    state.camera.lookAt(0, 0, 0);
+  });
+}
