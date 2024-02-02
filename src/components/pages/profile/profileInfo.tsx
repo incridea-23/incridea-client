d490952fe3f0d6ae0dfc0cd754dfc8bd658b0a71
nandanpi <nandanrp09@gmail.com>
2024-02-02 16:44:05 +0530

    Profile Page revamp

--- src/components/pages/profile/profileInfo.tsx
+++ src/components/pages/profile/profileInfo.tsx
@@ -52,8 +52,6 @@
 //               />
 //       </div>
 
-      
-
 //       {/* //contents ka div */}
 //       <div className='flex flex-col px-4'>
 //             <a className={`text-xl font-bold titleFont`}>
@@ -84,21 +82,19 @@
 
 //       </div>
 
-       
-
 //   </div>
 //   <div className='flex flex-row justify-around items-center w-full mt-5'>
 //       {/* //leaderboard and level div */}
 //       <div className='flex flex-row space-x-1'>
-          
+
 //                 <Image
 //                 src={'/assets/png/trophy.png'}
 //                 width={45}
 //                 height={45}
 //                 alt="map"
-               
+
 //                 />
-          
+
 //             <div className=''>
 //                 <p className='text-sm'>leaderboard</p>
 //                 <p>80/100</p>
@@ -106,15 +102,15 @@
 //       </div>
 
 //       <div className='flex flex-row space-x-1'>
-          
+
 //                 <Image
 //                 src={'/assets/png/money.png'}
 //                 width={45}
 //                 height={50}
 //                 alt="map"
-               
+
 //               />
-          
+
 //             <div className=''>
 //                 <p className='text-sm'>coins</p>
 //                 <p>100</p>
@@ -122,20 +118,20 @@
 //       </div>
 
 //       <div className='flex flex-row space-x-1'>
-          
+
 //                 <Image
 //                 src={'/assets/png/level.png'}
 //                 width={45}
 //                 height={45}
 //                 alt="map"
-                
+
 //               />
-          
+
 //             <div className=''>
 //                 <p className='text-sm'>level</p>
 //                 <p>3</p>
 //             </div>
-//       </div>       
+//       </div>
 //   </div>
 
 //   <div className='flex flex-col text-center text-white justify-center items-center mt-5'>
@@ -158,29 +154,32 @@
 // };
 
 // export default ProfileInfo;
-import { AccommodationRequestsByUserDocument, User } from '@/src/generated/generated';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
-import { QRCodeSVG } from 'qrcode.react';
-import { FC, useEffect, useState } from 'react';
-import { FaSignOutAlt, FaDice } from 'react-icons/fa';
-import { MdOutlineEmail, MdPhone } from 'react-icons/md';
-import TextAnimation from '../../animation/text';
-import { idToPid } from '@/src/utils/id';
-import Button from '../../button';
-import { signOut } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { useQuery } from '@apollo/client';
-import Spinner from '../../spinner';
-import ViewUserAccommodation from './viewUserAccommodation';
+import {
+  AccommodationRequestsByUserDocument,
+  User,
+} from "@/src/generated/generated";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { QRCodeSVG } from "qrcode.react";
+import { FC, useEffect, useState } from "react";
+import { FaSignOutAlt, FaDice } from "react-icons/fa";
+import { MdOutlineEmail, MdPhone } from "react-icons/md";
+import TextAnimation from "../../animation/text";
+import { idToPid } from "@/src/utils/id";
+import Button from "../../button";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import Spinner from "../../spinner";
+import ViewUserAccommodation from "./viewUserAccommodation";
 // import { GiShipWheel } from 'react-icons/gi';
 
 const ProfileInfo: FC<{
   user: User | null | undefined;
 }> = ({ user }) => {
   const router = useRouter();
-    const {
+  const {
     data: dataAccommodation,
     loading: loadingAccommodation,
     error: errorAccommodation,
@@ -188,153 +187,296 @@ const ProfileInfo: FC<{
 
   const [showModal, setShowModal] = useState(false);
 
-  if (user?.role === 'USER') {
-    router.push('/register');
+  if (user?.role === "USER") {
+    router.push("/register");
   }
 
+  //   return (
+  //     <section className="text-white h-full flex pt-20 lg:w-full lg:pt-0 justify-center items-center">
+
+  //  <div className='flex flex-col h-full px-2 lg:p-3 lg:pt-0 justify-center items-start'>
+  //   <div className='flex flex-row 2xl:flex-col items-center'>
+
+  //       {/* /profile picture ka div */}
+  //       <div className='justify-center items-start flex'>
+  //            <Image
+  //                 src={'/assets/png/c.png'}
+  //                 width={130}
+  //                 height={130}
+  //                 alt="map"
+  //               />
+  //       </div>
+
+  //       {/* //contents ka div */}
+  //       <div className='flex flex-col px-4'>
+  //             <a className={`text-xl font-bold titleFont`}>
+  //             {user?.name}
+  //           </a>
+  //           <a className="text-sm flex items-center gap-5 bodyFont">
+  //             {user?.college?.name || '-'}
+  //           </a>
+  // {/* <Button
+  //   onClick={() => signOut()}
+  //   className="mt-1 w-fit"
+  //   intent={'primary'}
+  // > <FaSignOutAlt className="inline-block mr-1" />
+  //   Sign Out
+  // </Button> */}
+  //       <ViewUserAccommodation
+  //   showModal={showModal}
+  //   setShowModal={setShowModal}
+  // />
+  //             {
+  //               loadingAccommodation ? (
+  //                 <Button size={'small'} onClick={()=>setShowModal(true)} className="w-max mt-3 md:mt-2"><Spinner size={'small'} className="text-[#dd5c6e]" /></Button>
+
+  //               ) : dataAccommodation?.accommodationRequestsByUser[0]?.status?
+  //               (<Button size={'small'} onClick={()=>setShowModal(true)} className="w-max mt-3 md:mt-2">View Request</Button>):
+  //             (<Button size={'small'} onClick={()=>router.push("/accommodation")} className="w-max mt-3 md:mt-2">Accommodate Me</Button>)
+  //             }
+
+  //       </div>
+
+  //   </div>
+  //   <div className='flex lg:flex-row justify-around space-x-5 items-center w-full  mt-3'>
+  //       {/* //leaderboard and level div */}
+  // <div className='flex flex-row'>
+
+  //           <Image
+  //           src={'/assets/png/trophy.png'}
+  //           width={45}
+  //           height={65}
+  //           alt="map"
+  //           className='h-full w-full'
+  //           />
+
+  //       <div className=''>
+  //           <p className='text-sm'>Leaderboard</p>
+  //           <p>80/100</p>
+  //       </div>
+  // </div>
+
+  // <div className='flex flex-row space-x-1'>
+
+  //           <Image
+  //           src={'/assets/png/coin.png'}
+  //           width={45}
+  //           height={50}
+  //           alt="map"
+
+  //         />
+
+  //       <div className=''>
+  //           <p className='text-sm'>Gold</p>
+  //           <p>100</p>
+  //       </div>
+  // </div>
+
+  // <div className='flex flex-row space-x-1'>
+
+  //           <Image
+  //           src={'/assets/png/level.png'}
+  //           width={45}
+  //           height={45}
+  //           alt="map"
+
+  //         />
+
+  //       <div className=''>
+  //           <p className='text-sm'>Level</p>
+  //           <p>3</p>
+  //       </div>
+  //       </div>
+  //   </div>
+
+  //   <div className='flex flex-row text-white justify-between items-center w-full px-3 mt-3'>
+  //       <div className='flex justify-end h-full flex-col'>
+  //       <p className="font-bold text-sm px-0">Contact details:</p>
+  //       <div className="flex gap-2 items-center">
+  //  <MdOutlineEmail />
+
+  //   {user?.email}
+  //            </div>
+  //             {user?.phoneNumber && (
+  //               <div className="flex gap-2 items-center">
+  //  <MdPhone />
+  //  {user?.phoneNumber}
+  //                </div>
+  //              )}
+  //            <div className=''>
+  //             <Button
+  //               onClick={() => signOut()}
+  //               className="mt-1 w-fit"
+  //               intent={'danger'}
+  //               size={'small'}
+  //             > <FaSignOutAlt className="inline-block #dc2626" />
+  //               Sign Out
+  //             </Button> </div>
+  //       </div>
+  //       {/* qr code div*/}
+  // <div>
+  //         <div className=''><QRCodeSVG
+  //           value={idToPid(user?.id!)}
+  //           size={110}
+  //           bgColor="transparent"
+  //           color="#ffffff"
+  //   fgColor="#ffffff"
+  //         /></div>
+  //          <a className={`titleFont text-[#fff] text-lg  mt-5`}>
+  //           {idToPid(user?.id!)}
+  //         </a>
+  //   </div>
+  //   </div>
+  // </div>
+  //     </section>
+  //   );
+
   return (
-    <section className="text-white h-full flex pt-20 lg:w-full lg:pt-0 justify-center items-center">
- 
-
- <div className='flex flex-col h-full px-2 lg:p-3 lg:pt-0 justify-center items-start'>
-  <div className='flex flex-row 2xl:flex-col items-center'>
-
-      {/* /profile picture ka div */}
-      <div className='justify-center items-start flex'>
-           <Image
-                src={'/assets/png/c.png'}
-                width={130}
-                height={130}
-                alt="map"
-              />
-      </div>
-
-      
-
-      {/* //contents ka div */}
-      <div className='flex flex-col px-4'>
-            <a className={`text-xl font-bold titleFont`}>
-            {user?.name}
-          </a>
-          <a className="text-sm flex items-center gap-5 bodyFont">
-            {user?.college?.name || '-'}
-          </a>
-            {/* <Button
-              onClick={() => signOut()}
-              className="mt-1 w-fit"
-              intent={'primary'}
-            > <FaSignOutAlt className="inline-block mr-1" />
-              Sign Out
-            </Button> */}
-            <ViewUserAccommodation
-        showModal={showModal}
-        setShowModal={setShowModal}
-      />
-            {
-              loadingAccommodation ? (
-                <Button size={'small'} onClick={()=>setShowModal(true)} className="w-max mt-3 md:mt-2"><Spinner size={'small'} className="text-[#dd5c6e]" /></Button>
-                
-              ) : dataAccommodation?.accommodationRequestsByUser[0]?.status?
-              (<Button size={'small'} onClick={()=>setShowModal(true)} className="w-max mt-3 md:mt-2">View Request</Button>):
-            (<Button size={'small'} onClick={()=>router.push("/accommodation")} className="w-max mt-3 md:mt-2">Accommodate Me</Button>)
-            }
-
-      </div>
-
-       
-
-  </div>
-  <div className='flex lg:flex-row justify-around space-x-5 items-center w-full  mt-3'>
-      {/* //leaderboard and level div */}
-      <div className='flex flex-row'>
-          
-                <Image
-                src={'/assets/png/trophy.png'}
-                width={45}
-                height={65}
-                alt="map"
-                className='h-full w-full'
-                />
-          
-            <div className=''>
-                <p className='text-sm'>Leaderboard</p>
-                <p>80/100</p>
-            </div>
-      </div>
-
-      <div className='flex flex-row space-x-1'>
-          
-                <Image
-                src={'/assets/png/coin.png'}
-                width={45}
-                height={50}
-                alt="map"
-               
-              />
-          
-            <div className=''>
-                <p className='text-sm'>Gold</p>
-                <p>100</p>
-            </div>
-      </div>
-
-      <div className='flex flex-row space-x-1'>
-          
-                <Image
-                src={'/assets/png/level.png'}
-                width={45}
-                height={45}
-                alt="map"
-                
-              />
-          
-            <div className=''>
-                <p className='text-sm'>Level</p>
-                <p>3</p>
-            </div>
-      </div>       
-  </div>
-
-  <div className='flex flex-row text-white justify-between items-center w-full px-3 mt-3'>
-      <div className='flex justify-end h-full flex-col'>
-      <p className="font-bold text-sm px-0">Contact details:</p>
-      <div className="flex gap-2 items-center">
-           <MdOutlineEmail />
-
-            {user?.email}
-           </div>
-            {user?.phoneNumber && (
-              <div className="flex gap-2 items-center">
-                 <MdPhone />
-                 {user?.phoneNumber}
-               </div>
-             )}
-           <div className=''>
-            <Button
-              onClick={() => signOut()}
-              className="mt-1 w-fit"
-              intent={'danger'}
-              size={'small'}
-            > <FaSignOutAlt className="inline-block #dc2626" />
-              Sign Out
-            </Button> </div>
-      </div>
-      {/* qr code div*/}
-      <div>
-              <div className=''><QRCodeSVG
-                value={idToPid(user?.id!)}
-                size={110}
-                bgColor="transparent"
-                color="#ffffff"
-        fgColor="#ffffff"
-              /></div>
-               <a className={`titleFont text-[#fff] text-lg  mt-5`}>
-                {idToPid(user?.id!)}
-              </a>
+    <>
+      <div className="text-white flex flex-col justify-between items-center h-full px-8 py-16 gap-y-8">
+        <div className="flex gap-5">
+          <Image
+            src={"/assets/png/avatar.png"}
+            width={200}
+            height={200}
+            alt="avatar"
+            className="lg:h-48 lg:w-48 md:h-36 md:w-36 w-28 h-28  rounded-full"
+          />
+          <section className="flex flex-col text-center h-full items-center justify-center space-y-4">
+            <span className="titleFont lg:text-[2.5rem] sm:text-3xl text-2xl  font-bold">
+              {user?.name}
+            </span>
+            <span className="bodyFont sm:text-xl text-md">
+              {user?.college?.name || "-"}
+            </span>
+          </section>
         </div>
-  </div>
-</div>
-    </section>
+
+        <div className="flex justify-evenly w-full titleFont basis-1/3  flex-wrap">
+          <div className="flex flex-row items-center space-x-2">
+            <Image
+              src={"/assets/png/trophy.png"}
+              width={100}
+              height={100}
+              alt="map"
+              className="sm:h-16 sm:w-16 h-12 w-12"
+            />
+
+            <div className="">
+              <p className="text-sm">Leaderboard</p>
+              <p>80/100</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row space-x-2 items-center titleFont">
+            <Image
+              src={"/assets/png/XP.webp"}
+              width={100}
+              height={100}
+              alt="map"
+              className="sm:h-16 sm:w-16 h-12 w-12"
+            />
+
+            <div className="">
+              <p className="text-sm">XP</p>
+              <p>100</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row space-x-1 items-center">
+            <Image
+              src={"/assets/png/level.png"}
+              width={100}
+              height={100}
+              alt="map"
+              className="sm:h-16 sm:w-16 h-12 w-12"
+            />
+
+            <div className="">
+              <p className="text-sm">Level</p>
+              <p>3</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex sm:flex-row flex-col-reverse gap-5 justify-between w-full items-center basis-1/2">
+          <section className="flex flex-col gap-y-4 sm:items-start items-center justify-center w-full">
+            <p className="flex flex-col justify-center gap-y-4">
+              <span className="sm:text-2xl text-xl text-center font-semibold">
+                Contact
+              </span>
+              <div className="flex flex-col gap-y-2 sm:text-lg text-md">
+                <span className="flex gap-x-2 items-center">
+                  <MdOutlineEmail />
+                  {user?.email}
+                </span>
+                <span className="flex gap-x-2 items-center">
+                  <MdPhone />
+                  {user?.phoneNumber}
+                </span>
+              </div>
+            </p>
+            <div>
+              <ViewUserAccommodation
+                showModal={showModal}
+                setShowModal={setShowModal}
+              />
+              {loadingAccommodation ? (
+                <Button
+                  size={"large"}
+                  onClick={() => setShowModal(true)}
+                  className="w-max mt-3 md:mt-2"
+                >
+                  <Spinner size={"small"} className="text-[#dd5c6e]" />
+                </Button>
+              ) : dataAccommodation?.accommodationRequestsByUser[0]?.status ? (
+                <Button
+                  intent={"info"}
+                  size={"large"}
+                  onClick={() => setShowModal(true)}
+                  className="w-max mt-3 md:mt-2"
+                >
+                  View Request
+                </Button>
+              ) : (
+                <Button
+                  intent={"success"}
+                  size={"large"}
+                  onClick={() => router.push("/accommodation")}
+                  className="w-max mt-3 md:mt-2"
+                >
+                  Accommodation
+                </Button>
+              )}
+
+              <Button
+                onClick={() => signOut()}
+                className="mt-1 w-full"
+                intent={"danger"}
+                size={"large"}
+              >
+                <FaSignOutAlt className="inline-block mr-1" />
+                Sign Out
+              </Button>
+            </div>
+          </section>
+
+          <div className="flex flex-col w-fit justify-center h-full items-center gap-y-3 border-2 shadow-md shadow-white border-primary-300 rounded-xl px-8 py-4">
+            <QRCodeSVG
+              value={idToPid(user?.id!)}
+              size={150}
+              bgColor="transparent"
+              color="#ffffff"
+              fgColor="#ffffff"
+              className="h-32 w-32"
+            />
+            <span className={`titleFont text-[#fff] sm:text-xl text-md`}>
+              {idToPid(user?.id!)}
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/components/pages/profile/registeredEvents.tsx
+++ src/components/pages/profile/registeredEvents.tsx
@@ -1,13 +1,13 @@
 // import { RegisterdEventsDocument } from '@/src/generated/generated';
 
-import { useQuery } from '@apollo/client';
-import Link from 'next/link';
-import Button from '../../button';
-import Spinner from '../../spinner';
-import EventCard from './eventCard';
-import UserTeams from './userTeams';
-import { FC } from 'react';
-import { RegisterdEventsDocument } from '@/src/generated/generated';
+import { useQuery } from "@apollo/client";
+import Link from "next/link";
+import Button from "../../button";
+import Spinner from "../../spinner";
+import EventCard from "./eventCard";
+import UserTeams from "./userTeams";
+import { FC } from "react";
+import { RegisterdEventsDocument } from "@/src/generated/generated";
 
 const UserEvents: FC<{
   userId: string;
@@ -17,45 +17,57 @@ const UserEvents: FC<{
   const { data: events, loading, error } = useQuery(RegisterdEventsDocument);
 
   return (
-    <section className='h-full mt-10 lg:mt-0'>
+    <section className="h-[50dvh]">
       {loading ? (
-        <div className="flex items-center justify-center h-40">
-          <Spinner size={'medium'} intent={'white'} />
+        <div className="flex items-center justify-center h-full">
+          <Spinner size={"medium"} intent={"white"} />
         </div>
       ) : events?.registeredEvents.__typename ===
-          'QueryRegisteredEventsSuccess' &&
+          "QueryRegisteredEventsSuccess" &&
         events.registeredEvents.data.length === 0 ? (
         <div className="flex flex-col items-center justify-center gap-5 h-full">
           <p className="text-white/80 text-lg">
             Register for an event to see it here gamer!
           </p>
-          <Button>
-            <Link href="/events" className="text-white">
-              Explore Quests
-            </Link>
-          </Button>
+          <Link href="/events" className="text-white">
+            <Button>Explore Quests</Button>
+          </Link>
         </div>
       ) : null}
 
       {!loading &&
         !error &&
         events?.registeredEvents.__typename ===
-          'QueryRegisteredEventsSuccess' &&
+          "QueryRegisteredEventsSuccess" &&
         events.registeredEvents.data.length !== 0 && (
           <>
-           {/* the mini bar */}
-            <div className='h-auto hidden lg:block w-full bg-[#ababab] backdrop-filter backdrop-blur-xl bg-opacity-10 font-bold text-xl md:p-3 text-white sticky top-0 z-50 text-center'>My Quests</div>
-            <div className='p-5'>
-            <h3 className='text-white px-3 z-1 md:text-center lg:text-left'>You have entered <span className='text-fuchsia-500'>{events.registeredEvents.data.length}</span> quest{events.registeredEvents.data.length > 1 && 's'}</h3>
-            <div className="flex gap-5 flex-wrap  pt-3 items-center justify-center lg:p-3">
-           
-            
-              {events?.registeredEvents.__typename ===
-                'QueryRegisteredEventsSuccess' &&
-                events?.registeredEvents.data?.map((event) => (
-                  <EventCard key={event.id} teams={event.teams} event={event} userId={userId} name={name} email={email} />
-                ))}
-            </div>
+            <div className="space-y-5 ">
+              <div className="titleFont text-center w-full rounded-t-xl text-white text-2xl py-2 bg-white/10">
+                My Quests
+              </div>
+              <div className="max-w-[95%] mx-auto space-y-4 pb-8">
+                <h3 className="text-white px-3 z-1 md:text-center lg:text-left">
+                  You have entered{" "}
+                  <span className="text-fuchsia-500">
+                    {events.registeredEvents.data.length}
+                  </span>{" "}
+                  quest{events.registeredEvents.data.length > 1 && "s"}
+                </h3>
+                <div className="grid  xl:grid-cols-2 grid-cols-1 gap-8 items-center justify-center">
+                  {events?.registeredEvents.__typename ===
+                    "QueryRegisteredEventsSuccess" &&
+                    events?.registeredEvents.data?.map((event) => (
+                      <EventCard
+                        key={event.id}
+                        teams={event.teams}
+                        event={event}
+                        userId={userId}
+                        name={name}
+                        email={email}
+                      />
+                    ))}
+                </div>
+              </div>
             </div>
           </>
         )}
