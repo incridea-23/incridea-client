b8e12bef3e91932af9c2b80c71838c2d579a9d6b
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-02-03 18:04:52 +0530

    feat: tool tip

---
+++ src/components/galleryslide/devices/tool-tip.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const ToolTip = ({
+  text,
+  classValue,
+}: {
+  text: string;
+  classValue: string;
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleMouseEnter = () => {
+    if (!isVisible && !isClicked) {
+      setIsVisible(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (isVisible && !isClicked) {
+      setIsVisible(false);
+    }
+  };
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setIsVisible(false);
+  };
+
+  return (
+    <div
+      className="absolute w-full h-full group top-0 z-[50] tracking-wide font-VikingHell flex justify-center items-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    >
+      <p
+        className={`${
+          isVisible ? "visible" : "invisible"
+        } md:w-[130px] w-[60px] py-2 px-4 text-white bg-black rounded-md z-50 absolute ${classValue}`}
+        style={{
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default ToolTip;
