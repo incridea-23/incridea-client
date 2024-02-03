5e1f8c2eae1768875792d30415e1a2ffc99d30f3
satwikrprabhu <satwikprabhu@gmail.com>
2024-02-03 12:07:14 +0530

    fix: special category

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -91,13 +91,13 @@ const Menu = () => {
   return (
     <div className="w-screen overflow-x-hidden flex flex-col absolute bottom-0 left-0 h-full justify-center items-center">
       <div className="lg:flex flex-col hidden  absolute bottom-10 items-center sm:flex-row  md:gap-10 my-24 gap-3  w-fit ">
-        <Button
+        <Link href="/login"><Button
           intent={"primary"}
           className="h-fit w-52  px-4 sm:px-12"
           size={"xlarge"}
         >
           Register
-        </Button>
+        </Button></Link>
         <Button
           intent={"ghost"}
           className="h-fit w-52 px-4 sm:px-12"
@@ -114,13 +114,13 @@ const Menu = () => {
         </h3>
         {
           <>
-            <Button
+            <Link href="/login"><Button
               intent={"ghost"}
               className="lg:hidden !bg-primary-800/70 block w-52 md:w-80 justify-center md:justify-end px-12 md:px-16"
               size={"xlarge"}
             >
               Register
-            </Button>
+            </Button></Link>
             <Button
               intent={"ghost"}
               className="lg:hidden !bg-primary-800/70 block w-52 md:w-80 justify-center md:justify-end px-12 md:px-16"
