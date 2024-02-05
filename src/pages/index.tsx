12b37e3a5c689f091f03a733556cdcc596826a51
nandanpi <nandanrp09@gmail.com>
2024-02-05 21:39:34 +0530

    Landing page & other minor fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,55 +36,8 @@ export default function Landing() {
     }
   }, [userXpLoading]);
 
-  // useGSAP(
-  //   () => {
-  //     gsap.to(landingContainer.current, {
-  //       scale: 13,
-  //       translateY: 550,
-  //       translateX: 200,
-  //       duration: 2,
-  //       delay: 0.5,
-  //       ease: "power2.in",
-  //       onComplete() {
-  //         gsap.to(landingContainer.current, { opacity: 0, duration: 1 });
-  //         setTimeout(() => {
-  //           setPageLoader(false);
-  //         }, 1000);
-  //       },
-  //     });
-  //   },
-  //   { scope: landingContainer }
-  // );
-
   return (
     <main className="h-screen relative overflow-hidden">
-      {/* {pageLoader && (
-        <section
-          ref={landingContainer}
-          className=" min-h-screen w-full flex justify-center items-center  z-[999] absolute top-0 left-0"
-        >
-          <Image
-            src={"/assets/landing/landing@2x.png"}
-            alt="UI Incridea 2024"
-            width={1920}
-            height={1080}
-            priority
-            className="image w-full h-full object-cover object-center absolute top-0 left-0"
-          />
-          <div className="aspect-video w-full relative h-screen min-w-max ">
-            <div className="absolute left-1/2 -translate-x-[60%] top-[39%] h-[12%] w-[8%]  ">
-              <Image
-                height={482}
-                width={256}
-                className="w-full h-full rounded-lg"
-                src="/assets/gif/nosignal.gif"
-                alt="no signal"
-                priority
-              />
-            </div>
-          </div>
-        </section>
-      )} */}
       {typeof window !== "undefined" && (
         <>
           {window.sessionStorage.getItem("arcadeLoader") ? null : (
