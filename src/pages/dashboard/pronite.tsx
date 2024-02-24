f769465067df71695842750b3f00db869955f096
satwikrprabhu <satwikprabhu@gmail.com>
2024-02-24 08:07:36 +0530

    fix: pronite color

--- src/pages/dashboard/pronite.tsx
+++ src/pages/dashboard/pronite.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Pronite() {
   return (
-    <div className="w-full min-h-screen p-5  bg-gradient-to-br from-[#001d67]  to-[#040c2b] flex flex-col justify-center items-center pt-20">
+    <div className="w-full min-h-screen p-5  bg-gradient-to-br from-[#400067]  to-[#16042b] flex flex-col justify-center items-center pt-20">
       <h2 className="mb-8 titleFont text-white md:text-4xl text-3xl">Pronite Scanner</h2>
       <div className="max-w-sm">
         <QRCodeScanner intent="pronite" />
